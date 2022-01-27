d9ac0ebb085f9b69c117047b30c2bb06b13d85f9
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-01-26 22:53:26 -0500

    list

--- resources/js/views/Crm.vue
+++ resources/js/views/Crm.vue
@@ -31,6 +31,37 @@
                     </div>
                 </TabList>
             </div>
+            <div class="items-center">
+                <Listbox as="div" v-model="selected">
+                    
+                <div class="mt-1 relative">
+                    
+                     <ListboxButton class="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <span class="block truncate">Hello</span>
+        <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+          <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </span>
+
+                    </ListboxButton>
+                    <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+        <ListboxOptions class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+          <ListboxOption as="template" v-for="person in people" :key="person.id" :value="person" v-slot="{ active, selected }">
+            <li :class="[active ? 'text-white bg-indigo-600' : 'text-gray-900', 'cursor-default select-none relative py-2 pl-3 pr-9']">
+              <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">
+                {{ person.name }}
+              </span>
+
+              <span v-if="selected" :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+              </span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+                </div>
+                
+                </Listbox>
+            </div>
             <div class="items-center px-2">
                <span class="relative z-0 inline-flex items-center h-full shadow-sm rounded-md">
                     <button type="button" class="relative inline-flex items-center py-1 px-4 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-indigo-600 hover:text-white focus-visible:z-10 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-indigo-500 focus-visible:border-indigo-500">
@@ -453,7 +484,20 @@
  import { Listbox, ListboxButton, ListboxOptions, ListboxOption, TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
  import StarRating from 'vue-star-rating'
  import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
- import { DotsVerticalIcon, ChevronDownIcon } from '@heroicons/vue/solid'
+ import { DotsVerticalIcon, ChevronDownIcon, CheckIcon, SearchCircleIcon } from '@heroicons/vue/solid'
+
+ const people = [
+  { id: 1, name: 'Wade Cooper' },
+  { id: 2, name: 'Arlene Mccoy' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+  { id: 5, name: 'Tanya Fox' },
+  { id: 6, name: 'Hellen Schmidt' },
+  { id: 7, name: 'Caroline Schultz' },
+  { id: 8, name: 'Mason Heaney' },
+  { id: 9, name: 'Claudie Smitham' },
+  { id: 10, name: 'Emil Schaefer' },
+]
 
 export default {
    
@@ -466,16 +510,17 @@ export default {
       TabPanel,
       StarRating,
       Listbox,
+      ListboxButton,
+      ListboxOptions,
+      ListboxOption,
       Menu,
       MenuButton,
       MenuItems,
       MenuItem,
       DotsVerticalIcon,
-      ChevronDownIcon
-      
-      
-    
-      
+      ChevronDownIcon,
+      CheckIcon,
+      SearchCircleIcon
     },
     data() {
         return {
@@ -646,12 +691,14 @@ export default {
                 
             ],
         }
-    }
+    },
+    
 }
  
 
 
 </script>
+
 
 <style scoped>
 
