479e06aa62b931042da65e782eb51624fcbd79a1
Aamish Irfan <aamishirfan2662@gmail.com>
2022-02-04 00:15:24 +0500

    images fix

--- resources/js/components/Home/HomeLogoCloud.vue
+++ resources/js/components/Home/HomeLogoCloud.vue
@@ -9,10 +9,10 @@
             <div class="flow-root self-center mt-8 lg:mt-0">
             <div class="-mt-4 -ml-8 flex flex-wrap justify-between lg:-ml-4">
                 <div class="mt-4 ml-8 flex flex-grow flex-shrink-0 justify-center lg:flex-grow-0 lg:ml-4">
-                <img class="h-10 opacity-30 saturate-0" :src="asset('\images\Logos\Zelf.svg')" alt="Zelf" />
+                <img class="h-10 opacity-30 saturate-0" :src="asset('images/Logos/Zelf.svg')" alt="Zelf" />
                 </div>
                 <div class="mt-4 ml-8 flex flex-grow flex-shrink-0 justify-center lg:flex-grow-0 lg:ml-4">
-                <img class="h-10 opacity-30 saturate-0" :src="asset('images\Logos\Fuego.svg')" alt="Fuego" />
+                <img class="h-10 opacity-30 saturate-0" :src="asset('images/Logos/Fuego.svg')" alt="Fuego" />
                 </div>
                 <div class="mt-4 ml-8 flex flex-grow flex-shrink-0 justify-center lg:flex-grow-0 lg:ml-4">
                 <img class="h-12 opacity-0 saturate-0" src="https://uploads-ssl.webflow.com/60cb5f56c3b31702dd6b1934/60cb64fa20a93418b234363f_Flowbo-Colors-p-500.png" alt="Flowbo" />
