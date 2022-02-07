4f4fea111c7026be316f871fcc698b3ee83c7ffb
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-02-07 10:25:23 -0500

    button

--- resources/js/components/Import/ImportColumnMatching.vue
+++ resources/js/components/Import/ImportColumnMatching.vue
@@ -1,38 +1,38 @@
 <template>
     <div class="items-center mx-auto min-h-screen">
-        <div class="mx-auto bg-white rounded-md shadow-lg shadow-indigo-700/20 max-w-7xl mt-12 py-8 items-center h-full justify-center ">
+        <div class="mx-auto bg-white rounded-md shadow-lg shadow-indigo-700/20 max-w-5xl mt-12 items-center h-full justify-center ">
         <div class="text-center py-4 font-bold text-md">Select columns to import</div>
-            <table class="w-full"> 
+            <table class="w-full rounded-md-b"> 
                 <tr class="bg-indigo-500 text-white">
-                    <th class="sr-only">Number</th>
-                    <th>Property</th>
-                    <th>Column</th>
+                    <th class="font-medium">Columns from <span class="font-bold">imports.csv</span></th>
+                    <th>Import to...</th>
                 </tr>
-                <tr v-for="column in columns" key="column.name" class="text-center odd:bg-white even:bg-indigo-100">
-                    <td class="text-sm w-6 text-neutral-400 font-bold pl-2 text-left mx-auto items-center">
-                        1
-                    </td> 
-                    <td class="text-sm text-indigo-700 font-bold py-2 text-left mx-auto items-center">
+                <tr v-for="column in columns" key="column.name" class="text-center rounded-md odd:bg-white even:bg-indigo-100">
+                    <td class="pl-4 justify-center items-center">
+                       
+                        <SelectMenu class="inline w-72"></SelectMenu>
+                    </td>  
+                    <td class="text-sm last:rounded-md-br text-indigo-700 font-bold py-2 text-center mx-auto items-center">
                         {{column.name}}
                     </td> 
-                    <td class="pl-4 items-center">
-                        <SelectMenu></SelectMenu>
-                    </td>  
                 </tr>                        
             </table>
+            <ButtonGroup :text="Finish"></ButtonGroup>
         </div>
     </div>
     
 </template>
 <script>
 import SelectMenu from "../SelectMenu.vue";
+import ButtonGroup from "../ButtonGroup.vue";
 
 
 
 export default {
     name: "ImportColumnMatching",
     components: {
-        SelectMenu
+        SelectMenu,
+        ButtonGroup,
     },
     data() {
         return {
