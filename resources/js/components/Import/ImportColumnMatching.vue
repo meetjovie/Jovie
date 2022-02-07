8763e1dbf9e6d4e1d388a63428c37558b0e1d706
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-02-07 10:07:52 -0500

    columns

--- resources/js/components/Import/ImportColumnMatching.vue
+++ resources/js/components/Import/ImportColumnMatching.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="items-center mx-auto min-h-screen">
-        <div class="mx-auto rounded-md shadow-lg shadow-indigo-700/20 px-8 max-w-7xl mt-8 py-24 items-center h-full justify-center ">
-            <div v-for="column in columns" key="column.name" class="grid grid-cols-2">   
-                <div class="text-sm text-indigo-700 font-bold px-8 text-center mx-auto items-center">
-                    {{column.name}}
-                </div> 
-                <div class="px-4">
-                    <SelectMenu></SelectMenu>
-                </div>                        
-            </div>
+        <div class="mx-auto bg-neutral-100 rounded-md shadow-lg shadow-indigo-700/20 px-8 max-w-7xl mt-12 py-8 items-center h-full justify-center ">
+        <div class="text-center py-4 font-bold text-md">Select columns to import</div>
+            <table class="w-full"> 
+                <tr class="bg-neutral-400">
+                    <th>Property</th>
+                    <th>Column</th>
+                </tr>
+                <tr v-for="column in columns" key="column.name" class="text-center px-4 odd:bg-white even:bg-indigo-100">
+                    <td class="text-sm text-indigo-700 font-bold pl-8 text-left mx-auto items-center">
+                        {{column.name}}
+                    </td> 
+                    <td class="pl-4 items-center">
+                        <SelectMenu></SelectMenu>
+                    </td>  
+                </tr>                        
+            </table>
         </div>
     </div>
+    
 </template>
 <script>
 import SelectMenu from "../SelectMenu.vue";
