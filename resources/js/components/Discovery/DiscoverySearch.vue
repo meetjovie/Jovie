38125d50b230f64b5ea5dcd91686fc76470f4155
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-02-07 12:42:47 -0500

    location

--- resources/js/components/Discovery/DiscoverySearch.vue
+++ resources/js/components/Discovery/DiscoverySearch.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="grid grid-cols-4 py-4 px-2 gap-y-2">
-        <InputGroup class="col-span-4" :icon="PhotographIcon" :name="ContentSearch" :placeholder="Search" :label="Search" :type="input" />
+        <InputGroup class="col-span-4" icon="PhotographIcon" text="ContentSearch" placeholder="Search" label="Search" type="input" />
         <div>Network1</div>
         <div>Content Search</div>
         <div>Tags and Categories</div>
-        <div>Location</div>
+        <InputGroup class="col-span-4" icon="LocationMarkerIcon" placeholder="Location" />
         <div>Gender</div>
-        <ButtonGroup :icon="SearchIcon"/>
-        <MultiButton :name="Email"/>
+        
+    
         <div>Verified, private, favorite, muted</div>
         <div>Email</div>
         <div>Engagment Filter</div>
         <div>Follower Count<br>with EF toggle</div>
         <div>Brand Mentions</div>
         <div class="col-span-4 justify-right text-right">
-            <ButtonGroup :icon="SearchIcon" :name="search" class="w-full">Search</ButtonGroup>
+            <ButtonGroup text="Search" class="w-full">Search</ButtonGroup>
             <span class="text-indigo-500 justify-right text-xs text-right cursor-pointer">Reset</span>
         </div>
     </div>
@@ -30,9 +30,6 @@ export default {
         InputGroup,
         ButtonGroup,
         MultiButton,
-       
-
-
 
     },
 }
