e054eac6d22c29a0ee199ff0621df1331fbe5990
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-02-07 09:54:28 -0500

    design

--- resources/js/components/Import/ImportColumnMatching.vue
+++ resources/js/components/Import/ImportColumnMatching.vue
@@ -1,16 +1,14 @@
 <template>
     <div class="items-center mx-auto min-h-screen">
-        <div class="mx-auto mt-8 py-24 items-center h-full justify-center grid gap-x-4 grid-flow-col auto-cols-max">
-            <div v-for="column in columns" key="column.name">
-                        <div class="rounded-md shadow-lg shadow-indigo-700/20">
-                            <div class="bg-white rounded-t-md text-sm text-indigo-700 font-bold px-8 text-center mx-auto border-b border border-neutral-300 py-2">{{column.name}}</div>
-                            <div class="w-72 py-6 px-4">
-                                <SelectMenu></SelectMenu>
-                            </div>
-                            <div class="cursor-pointer hover:bg-neutral-700 active:text-white active:bg-indigo-500 hover:text-white rounded-b-md px-8 py-2 font-bold text-neutral-400 text-center text-xs">Skip</div>
-                        </div>
+        <div class="mx-auto rounded-md shadow-lg shadow-indigo-700/20 px-8 max-w-7xl mt-8 py-24 items-center h-full justify-center ">
+            <div v-for="column in columns" key="column.name" class="grid grid-cols-2">   
+                <div class="text-sm text-indigo-700 font-bold px-8 text-center mx-auto items-center">
+                    {{column.name}}
+                </div> 
+                <div class="px-4">
+                    <SelectMenu></SelectMenu>
+                </div>                        
             </div>
-
         </div>
     </div>
 </template>
