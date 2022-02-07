3bab70bb728445c5879d819db01a6d3e35cb39a4
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-02-07 10:12:14 -0500

    columns

--- resources/js/components/Import/ImportColumnMatching.vue
+++ resources/js/components/Import/ImportColumnMatching.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="items-center mx-auto min-h-screen">
-        <div class="mx-auto bg-neutral-100 rounded-md shadow-lg shadow-indigo-700/20 px-8 max-w-7xl mt-12 py-8 items-center h-full justify-center ">
+        <div class="mx-auto bg-white rounded-md shadow-lg shadow-indigo-700/20 max-w-7xl mt-12 py-8 items-center h-full justify-center ">
         <div class="text-center py-4 font-bold text-md">Select columns to import</div>
             <table class="w-full"> 
-                <tr class="bg-neutral-400">
+                <tr class="bg-indigo-500 text-white">
+                    <th class="sr-only">Number</th>
                     <th>Property</th>
                     <th>Column</th>
                 </tr>
-                <tr v-for="column in columns" key="column.name" class="text-center px-4 odd:bg-white even:bg-indigo-100">
-                    <td class="text-sm text-indigo-700 font-bold pl-8 text-left mx-auto items-center">
+                <tr v-for="column in columns" key="column.name" class="text-center odd:bg-white even:bg-indigo-100">
+                    <td class="text-sm w-6 text-neutral-400 font-bold pl-2 text-left mx-auto items-center">
+                        1
+                    </td> 
+                    <td class="text-sm text-indigo-700 font-bold py-2 text-left mx-auto items-center">
                         {{column.name}}
                     </td> 
                     <td class="pl-4 items-center">
