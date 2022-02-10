4b5f9280e9559b29ff383379731fddca19a754c5
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-02-10 13:59:56 -0500

    added mega menu

--- resources/js/components/External/ExternalHeader.vue
+++ resources/js/components/External/ExternalHeader.vue
@@ -56,7 +56,9 @@
                                                     <div
                                                     class="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white sm:h-12 sm:w-12"
                                                     >
-                                                    <div v-html="item.icon"></div>
+                                                    <div class="bg-indigo-300 rounded-md p-2">
+                                                        <component :is="item.icon" class="h-6 w-6 text-indigo-700"/>
+                                                    </div>
                                                     </div>
                                                     <div class="ml-4">
                                                     <p class="text-sm font-medium text-gray-900">
@@ -69,8 +71,8 @@
                                                 </a>
                                                 </div>
                                                 <div class="p-4 bg-gray-50">
-                                                <a
-                                                    href="##"
+                                                <router-link
+                                                    to="demo"
                                                     class="flow-root px-2 py-2 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-indigo-500 focus-visible:ring-opacity-50"
                                                 >
                                                     <span class="flex items-center">
@@ -81,7 +83,7 @@
                                                     <span class="block text-sm text-gray-500">
                                                     Wanna see what Jovie can do?
                                                     </span>
-                                                </a>
+                                                </router-link>
                                                 </div>
                                             </div>
                                             </PopoverPanel>
@@ -166,10 +168,10 @@
 </template>
 <script>
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
-import { ChevronDownIcon } from '@heroicons/vue/solid'
+import { ChevronDownIcon, UserGroupIcon, SearchIcon, MailIcon } from '@heroicons/vue/outline'
 
 export default {
-  components: { Popover, PopoverButton, PopoverPanel, ChevronDownIcon },
+  components: { Popover, PopoverButton, PopoverPanel, ChevronDownIcon, UserGroupIcon, SearchIcon, MailIcon },
 
   data() {
     return {
@@ -178,87 +180,19 @@ export default {
           name: 'AI Discovery',
           description: 'Identify creators most likely to love your brand ',
           href: '##',
-          icon: `
-            <svg
-              width="48"
-              height="48"
-              viewBox="0 0 48 48"
-              fill="none"
-              aria-hidden='true'
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-              <path
-                d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
-                stroke="#FB923C"
-                stroke-width="2"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
-                stroke="#FDBA74"
-                stroke-width="2"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
-                stroke="#FDBA74"
-                stroke-width="2"
-              />
-            </svg>
-          `,
+          icon: 'SearchIcon',
         },
         {
           name: 'CRM',
           description: 'Manage your relationships',
           href: '##',
-          icon: `
-            <svg
-              width="48"
-              height="48"
-              viewBox="0 0 48 48"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-              <path
-                d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
-                stroke="#FB923C"
-                stroke-width="2"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
-                stroke="#FDBA74"
-                stroke-width="2"
-              />
-            </svg>
-          `,
+          icon: 'UserGroupIcon',
         },
         {
           name: 'Outreach',
           description: 'Onboard new creators at scale',
           href: '##',
-          icon: `
-            <svg
-              width="48"
-              height="48"
-              viewBox="0 0 48 48"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-              <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
-              <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
-              <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
-              <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
-              <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
-              <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
-            </svg>
-          `,
+          icon: 'MailIcon',
         },
       ],
     }
