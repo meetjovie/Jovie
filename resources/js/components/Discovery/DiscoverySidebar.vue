aef707a96a9f4fe9f7d3d2a122b39e0f6e32ffe6
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-03-05 13:07:34 -0500

    tabs

--- resources/js/components/Discovery/DiscoverySidebar.vue
+++ resources/js/components/Discovery/DiscoverySidebar.vue
@@ -149,56 +149,7 @@
       </div>
 
       <div class="border-b px-4"></div>
-      <div class="left-92 absolute w-192"></div>
-      <div class="group grid grid-cols-3 justify-center">
-        <img
-          src="https://picsum.photos/220/300"
-          class="mx-auto aspect-square h-full justify-center object-cover object-center" />
-        <img
-          src="https://picsum.photos/240/300"
-          class="mx-auto aspect-square h-full justify-center object-cover object-center" />
-        <img
-          src="https://picsum.photos/230/300"
-          class="mx-auto aspect-square h-full justify-center object-cover object-center" />
-      </div>
-      <div class="group mx-auto grid grid-cols-3 justify-center">
-        <div
-          class="flex justify-between border-t border-b border-neutral-200 bg-neutral-200/20 px-6 py-0.5 text-center text-neutral-700 backdrop-blur-xl backdrop-saturate-150 backdrop-filter">
-          <div class="inline-flex text-xs">
-            <ThumbUpIcon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-          <div class="inline-flex text-xs">
-            <PlayIcon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-          <div class="inline-flex text-xs">
-            <ChatAlt2Icon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-        </div>
-        <div
-          class="flex justify-between border-t border-b border-neutral-200 bg-neutral-200/20 px-6 py-0.5 text-center text-neutral-700 backdrop-blur-xl backdrop-saturate-150 backdrop-filter">
-          <div class="inline-flex text-xs">
-            <ThumbUpIcon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-          <div class="inline-flex text-xs">
-            <PlayIcon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-          <div class="inline-flex text-xs">
-            <ChatAlt2Icon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-        </div>
-        <div
-          class="flex justify-between border-t border-b border-neutral-200 bg-neutral-200/20 px-6 py-0.5 text-center text-neutral-700 backdrop-blur-xl backdrop-saturate-150 backdrop-filter">
-          <div class="inline-flex text-xs">
-            <ThumbUpIcon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-          <div class="inline-flex text-xs">
-            <PlayIcon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-          <div class="inline-flex text-xs">
-            <ChatAlt2Icon class="mt-0.5 mr-0.5 h-3 w-3" /> 34K
-          </div>
-        </div>
-      </div>
+      <CreatorContentBar />
     </PopoverPanel>
   </transition>
 </template>
@@ -214,6 +165,7 @@ import {
   MenuItem,
 } from '@headlessui/vue';
 import CreatorAvatar from '../Creator/CreatorAvatar.vue';
+import CreatorContentBar from '../Creator/CreatorContentBar.vue';
 import CreatorSocialLinks from '../Creator/CreatorSocialLinks.vue';
 import {
   XIcon,
@@ -262,6 +214,7 @@ export default {
     PlayIcon,
     ChatAlt2Icon,
     LocationMarkerIcon,
+    CreatorContentBar,
   },
   data() {
     return {
--- resources/js/views/CreatorOverview.vue
+++ resources/js/views/CreatorOverview.vue
@@ -109,18 +109,6 @@
                     </dd>
                   </div>
                   <div class="sm:col-span-1">
-                    <dt class="text-sm font-medium text-gray-500">Category</dt>
-                    <dd class="mt-1 text-sm text-gray-900">
-                      <CreatorTags size="md" text="Category" />
-                    </dd>
-                  </div>
-                  <div class="sm:col-span-1">
-                    <dt class="text-sm font-medium text-gray-500">
-                      Salary expectation
-                    </dt>
-                    <dd class="mt-1 text-sm text-gray-900">$120,000</dd>
-                  </div>
-                  <div class="sm:col-span-1">
                     <dt class="text-sm font-medium text-gray-500">Stage</dt>
                     <dd class="mt-1 text-sm text-gray-900">
                       <Popover as="div" class="relative inline-block text-left">
@@ -164,6 +152,18 @@
                           </PopoverPanel>
                         </transition>
                       </Popover>
+                    </dd>
+                  </div>
+                  <div class="sm:col-span-1">
+                    <dt class="text-sm font-medium text-gray-500">
+                      Last contact
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900">3 days ago</dd>
+                  </div>
+                  <div class="sm:col-span-1">
+                    <dt class="text-sm font-medium text-gray-500">Category</dt>
+                    <dd class="mt-1 text-sm text-gray-900">
+                      <CreatorTags size="md" text="Category" />
                     </dd>
                   </div>
                   <div class="sm:col-span-2">
@@ -271,18 +271,15 @@
           aria-labelledby="timeline-title"
           class="lg:col-span-2 lg:col-start-2">
           <div class="bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6">
-            <h2 id="timeline-title" class="text-lg font-medium text-gray-900">
-              Timeline
-            </h2>
+            <TabGroup as="div">
+              <!-- Links -->
 
-            <!-- Links -->
-            <TabGroup as="div" class="mt-2">
               <div class="border-b border-gray-200">
                 <TabList class="-mb-px flex space-x-8 px-4">
                   <Tab
                     as="template"
-                    v-for="navLink in nav"
-                    :key="navLink.name"
+                    v-for="tab in tabs"
+                    :key="tab.name"
                     v-slot="{ selected }">
                     <button
                       :class="[
@@ -291,232 +288,24 @@
                           : 'border-transparent text-gray-900',
                         'flex-1 whitespace-nowrap border-b-2 py-4 px-1 text-base font-medium',
                       ]">
-                      {{ navLink.name }}
+                      {{ tab.name }}
                     </button>
                   </Tab>
                 </TabList>
               </div>
               <TabPanels as="template">
                 <TabPanel class="space-y-12 px-4 pt-10 pb-6">
-                  Panel content
+                  <ActivityFeed button="Show more" />
+                </TabPanel>
+                <TabPanel class="space-y-12 px-4 pt-10 pb-6">
+                  <CreatorContentBar />
+                </TabPanel>
+                <TabPanel class="space-y-12 px-4 pt-10 pb-6">
+                  <CommentThread />
+                  <CommentBox />
                 </TabPanel>
               </TabPanels>
             </TabGroup>
-
-            <!-- Activity Feed -->
-            <div class="mt-6 flow-root">
-              <ul role="list" class="-mb-8">
-                <li>
-                  <div class="relative pb-8">
-                    <span
-                      class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                      aria-hidden="true"></span>
-                    <div class="relative flex space-x-3">
-                      <div>
-                        <span
-                          class="flex h-8 w-8 items-center justify-center rounded-full bg-gray-400 ring-8 ring-white">
-                          <!-- Heroicon name: solid/user -->
-                          <svg
-                            class="h-5 w-5 text-white"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true">
-                            <path
-                              fill-rule="evenodd"
-                              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                              clip-rule="evenodd" />
-                          </svg>
-                        </span>
-                      </div>
-                      <div
-                        class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
-                        <div>
-                          <p class="text-sm text-gray-500">
-                            Applied to
-                            <a href="#" class="font-medium text-gray-900"
-                              >Front End Developer</a
-                            >
-                          </p>
-                        </div>
-                        <div
-                          class="whitespace-nowrap text-right text-sm text-gray-500">
-                          <time datetime="2020-09-20">Sep 20</time>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-
-                <li>
-                  <div class="relative pb-8">
-                    <span
-                      class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                      aria-hidden="true"></span>
-                    <div class="relative flex space-x-3">
-                      <div>
-                        <span
-                          class="flex h-8 w-8 items-center justify-center rounded-full bg-indigo-500 ring-8 ring-white">
-                          <!-- Heroicon name: solid/thumb-up -->
-                          <svg
-                            class="h-5 w-5 text-white"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true">
-                            <path
-                              d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
-                          </svg>
-                        </span>
-                      </div>
-                      <div
-                        class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
-                        <div>
-                          <p class="text-sm text-gray-500">
-                            Advanced to phone screening by
-                            <a href="#" class="font-medium text-gray-900"
-                              >Bethany Blake</a
-                            >
-                          </p>
-                        </div>
-                        <div
-                          class="whitespace-nowrap text-right text-sm text-gray-500">
-                          <time datetime="2020-09-22">Sep 22</time>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-
-                <li>
-                  <div class="relative pb-8">
-                    <span
-                      class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                      aria-hidden="true"></span>
-                    <div class="relative flex space-x-3">
-                      <div>
-                        <span
-                          class="flex h-8 w-8 items-center justify-center rounded-full bg-green-500 ring-8 ring-white">
-                          <!-- Heroicon name: solid/check -->
-                          <svg
-                            class="h-5 w-5 text-white"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true">
-                            <path
-                              fill-rule="evenodd"
-                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                              clip-rule="evenodd" />
-                          </svg>
-                        </span>
-                      </div>
-                      <div
-                        class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
-                        <div>
-                          <p class="text-sm text-gray-500">
-                            Completed phone screening with
-                            <a href="#" class="font-medium text-gray-900"
-                              >Martha Gardner</a
-                            >
-                          </p>
-                        </div>
-                        <div
-                          class="whitespace-nowrap text-right text-sm text-gray-500">
-                          <time datetime="2020-09-28">Sep 28</time>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-
-                <li>
-                  <div class="relative pb-8">
-                    <span
-                      class="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                      aria-hidden="true"></span>
-                    <div class="relative flex space-x-3">
-                      <div>
-                        <span
-                          class="flex h-8 w-8 items-center justify-center rounded-full bg-indigo-500 ring-8 ring-white">
-                          <!-- Heroicon name: solid/thumb-up -->
-                          <svg
-                            class="h-5 w-5 text-white"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true">
-                            <path
-                              d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
-                          </svg>
-                        </span>
-                      </div>
-                      <div
-                        class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
-                        <div>
-                          <p class="text-sm text-gray-500">
-                            Advanced to interview by
-                            <a href="#" class="font-medium text-gray-900"
-                              >Bethany Blake</a
-                            >
-                          </p>
-                        </div>
-                        <div
-                          class="whitespace-nowrap text-right text-sm text-gray-500">
-                          <time datetime="2020-09-30">Sep 30</time>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-
-                <li>
-                  <div class="relative pb-8">
-                    <div class="relative flex space-x-3">
-                      <div>
-                        <span
-                          class="flex h-8 w-8 items-center justify-center rounded-full bg-green-500 ring-8 ring-white">
-                          <!-- Heroicon name: solid/check -->
-                          <svg
-                            class="h-5 w-5 text-white"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true">
-                            <path
-                              fill-rule="evenodd"
-                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                              clip-rule="evenodd" />
-                          </svg>
-                        </span>
-                      </div>
-                      <div
-                        class="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
-                        <div>
-                          <p class="text-sm text-gray-500">
-                            Completed interview with
-                            <a href="#" class="font-medium text-gray-900"
-                              >Katherine Snyder</a
-                            >
-                          </p>
-                        </div>
-                        <div
-                          class="whitespace-nowrap text-right text-sm text-gray-500">
-                          <time datetime="2020-10-04">Oct 4</time>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-            <div class="justify-stretch mt-6 flex flex-col">
-              <button
-                type="button"
-                class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                Advance to offer
-              </button>
-            </div>
           </div>
         </section>
       </div>
@@ -525,9 +314,12 @@
 </template>
 <script>
 import CreatorAvatar from '../components/Creator/CreatorAvatar.vue';
+import CreatorContentBar from '../components/Creator/CreatorContentBar.vue';
 import SocialIcons from '../components/SocialIcons.vue';
 import InputGroup from '../components/InputGroup.vue';
 import { PlusIcon } from '@heroicons/vue/solid';
+import CommentBox from '../components/CommentBox.vue';
+import ActivityFeed from '../components/ActivityFeed.vue';
 import {
   Tab,
   TabGroup,
@@ -543,6 +335,7 @@ import {
 } from '@headlessui/vue';
 import CreatorTags from '../components/Creator/CreatorTags.vue';
 import StarRating from 'vue-star-rating';
+import CommentThread from '../components/CommentThread.vue';
 import CreatorHandles from '../components/Creator/CreatorHandles.vue';
 
 export default {
@@ -565,6 +358,10 @@ export default {
     PopoverButton,
     PopoverPanel,
     PopoverGroup,
+    CreatorContentBar,
+    CommentBox,
+    ActivityFeed,
+    CommentThread,
   },
   data() {
     return {
@@ -585,6 +382,18 @@ export default {
           key: '2',
         },
       ],
+      tabs: [
+        {
+          name: 'Activity',
+        },
+        {
+          name: 'Content',
+        },
+        {
+          name: 'Comments',
+        },
+      ],
+
       socials: [
         { icon: 'UserIcon', size: '24', placeholder: '@itstimwhtie' },
         { icon: 'instagram', size: '24', placeholder: '@timwhite' },
