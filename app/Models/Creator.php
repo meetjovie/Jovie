d21f2808c1e548674d1cc8f0abfdbcfc42b18654
Aamish Irfan <aamishirfan2662@gmail.com>
2022-03-14 02:59:11 +0500

    twitter fixes

--- app/Models/Creator.php
+++ app/Models/Creator.php
@@ -147,6 +147,28 @@ class Creator extends Model
         }
     }
 
+    public function getInstagramHandlerAttribute($value)
+    {
+        if ($value) {
+            return 'https://instagram.com/' . $value;
+        }
+        return null;
+    }
+
+    public function getTwitterMetaAttribute($value)
+    {
+        return json_decode($value ?? '{}');
+    }
+
+    public function brands()
+    {
+        return $this->belongsToMany(Creator::class, 'brand_creator',
+            'creator_id',
+            'brand_id',
+            'id',
+            'id')->withTimestamps();
+    }
+
     public function getYoutubeHandlerAttribute($value)
     {
         return json_decode($value ?? '{}');
@@ -177,21 +199,29 @@ class Creator extends Model
         return json_decode($value ?? '[]');
     }
 
-    public function getInstagramHandlerAttribute($value)
+    public function setEmailsAttribute($value)
     {
-        if ($value) {
-            return 'https://instagram.com/' . $value;
-        }
-        return null;
+        $this->attributes['emails'] = json_encode($value ?? []);
     }
 
-    public function brands()
+    public function setTagsAttribute($value)
     {
-        return $this->belongsToMany(Creator::class, 'brand_creator',
-            'creator_id',
-            'brand_id',
-            'id',
-            'id')->withTimestamps();
+        $this->attributes['tags'] = json_encode($value ?? []);
+    }
+
+    public function setInstagramMediaAttribute($value)
+    {
+        $this->attributes['instagram_media'] = json_encode($value ?? []);
+    }
+
+    public function setInstagramMetaAttribute($value)
+    {
+        $this->attributes['instagram_meta'] = json_encode($value ?? []);
+    }
+
+    public function setSocialLinksAttribute($value)
+    {
+        $this->attributes['social_links'] = json_encode($value ?? []);
     }
 
     public static function getCrmCreators($params)
@@ -240,8 +270,10 @@ class Creator extends Model
             ->get()->keyBy('creator_id');
 
         foreach ($creators as &$creator) {
-            if (!$creator->crmRecordByUser->instagram_offer) {
-                $creator->crmRecordByUser->instagram_suggested_offer = round($creator->instagram_meta->engaged_follows * 0.5, 0);
+            foreach (Creator::NETWORKS as $network) {
+                if (!$creator->crmRecordByUser[$network.'_offer'] && count((array) $creator[$network.'_meta'])) {
+                    $creator->crmRecordByUser[$network.'_suggested_offer'] = round(($creator[$network.'_meta']->engaged_follows ?? 0) * 0.5, 0);
+                }
             }
             if (!$creator->crmRecordByUser->rating && isset($avgRatings[$creator->id])) {
                 $creator->crmRecordByUser->average_rating = round($avgRatings[$creator->id]->average_rating);
--- resources/js/components/Crm/CrmTable.vue
+++ resources/js/components/Crm/CrmTable.vue
@@ -223,7 +223,7 @@
                                                             id="creator-offer"
                                                             class="block w-full bg-white/0 px-2 py-1 placeholder-neutral-300 focus-visible:border-2 focus:border-indigo-700 focus-visible:border-indigo-500 focus-visible:ring-indigo-500 sm:text-xs"
 
-                                                            :placeholder="creator.crm_record_by_user.instagram_suggested_offer"
+                                                            :placeholder="creator.crm_record_by_user[`${network}_suggested_offer`]"
                                                             aria-describedby="email-description"/>
                                                       </span>
                         </td>
@@ -241,7 +241,7 @@
                                 <PopoverButton
                                     class="group my-0 inline-flex w-32 items-center justify-between rounded-sm bg-blue-100 px-2 py-1 text-xs font-semibold leading-5 text-blue-800">
                                     {{
-                                        creator.crm_record_by_user[`${network}_stage`]
+                                        creator.crm_record_by_user.stage
                                     }}
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
@@ -269,8 +269,8 @@
                                             <div class="">
                                                 <button
                                                     v-for="(stage, key) in stages"
-                                                    @click="$emit('updateCreator', {id:creator.id, index: index, network: network, key: `crm_record_by_user.${network}_stage`, value: key})"
-                                                    :class="[creator.crm_record_by_user[`${network}_stage`] == key ? 'bg-indigo-500 text-neutral-700': 'text-gray-900','flex w-full items-center group first:rounded-t-lg last:rounded-b-lg px-2 py-2 first:pt-2 last:pb-2 text-neutral-700 hover:bg-indigo-700 hover:text-white text-xs']">
+                                                    @click="$emit('updateCreator', {id:creator.id, index: index, network: network, key: `crm_record_by_user.stage`, value: key})"
+                                                    :class="[creator.crm_record_by_user.stage == key ? 'bg-indigo-500 text-neutral-700': 'text-gray-900','flex w-full items-center group first:rounded-t-lg last:rounded-b-lg px-2 py-2 first:pt-2 last:pb-2 text-neutral-700 hover:bg-indigo-700 hover:text-white text-xs']">
                                                     <div
                                                         class="mr-2 font-bold  opacity-50">
                                                         {{ key + 1 }}
