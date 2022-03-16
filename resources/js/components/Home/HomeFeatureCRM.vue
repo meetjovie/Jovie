60ba0ee28facdc23d7d2aa8b98988d372108f913
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-03-16 17:24:42 -0400

    type

--- resources/js/components/Home/HomeFeatureCRM.vue
+++ resources/js/components/Home/HomeFeatureCRM.vue
@@ -1,18 +1,28 @@
 <template>
-            <div class="relative bg-gray-50 pt-16 overflow-hidden sm:pt-24 lg:pt-32">
-                <div class="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
-                <div>
-                    <h2 class="text-base font-semibold tracking-wider text-indigo-600 uppercase">Manage your creator relationships</h2>
-                    <p class="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-                    A CRM built for the <span class="text-indigo-700">creator economy</span>.
-                    </p>
-                    <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-                    Jovie's CRM and pipeline tracker give you creator specific sales intelligence allowing you to prioritize the relationships most valuable to your business.
-                    </p>
-                </div>
-                <div class="mt-12 -mb-10 sm:-mb-24 lg:-mb-80">
-                    <img class="rounded-lg shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5" :src="asset('img/External/HomeFeatureCRM.webp')" />
-                </div>
-                </div>
-            </div>  
+  <div class="relative overflow-hidden bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
+    <div
+      class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+      <div>
+        <h2
+          class="text-base font-semibold uppercase tracking-wider text-indigo-600">
+          Manage your creator relationships
+        </h2>
+        <p
+          class="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          The operating system for the
+          <span class="text-indigo-700">creator economy</span>.
+        </p>
+        <p class="mx-auto mt-5 max-w-prose text-xl text-gray-500">
+          Jovie's CRM, pipeline tracker, and social insights give you creator
+          specific intelligence allowing you to prioritize the relationships
+          most valuable to your business.
+        </p>
+      </div>
+      <div class="mt-12 -mb-10 sm:-mb-24 lg:-mb-80">
+        <img
+          class="rounded-lg shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5"
+          :src="asset('img/External/HomeFeatureCRM.webp')" />
+      </div>
+    </div>
+  </div>
 </template>
