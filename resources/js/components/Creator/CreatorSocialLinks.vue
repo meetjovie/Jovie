76fe674603eda4f0f397cf4631234019719ca24f
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-03-17 19:07:47 -0400

    added sse tags

--- resources/js/components/Creator/CreatorSocialLinks.vue
+++ resources/js/components/Creator/CreatorSocialLinks.vue
@@ -1,52 +1,62 @@
 <template>
-    <div>
-        <div
-            v-if="iconstyle == 'vertical'"
-            class="mt-1 grid grid-cols-6 items-center text-center">
-            <div
-                v-for="socialLink in socialLinks"
-                :key="socialLink.id" @click="openLink(socialLink.url, true)"
-                class="mx-auto items-center justify-center cursor-pointer">
-                <div class="text-neutral-5 mx-auto justify-center text-center">
-                    <SocialIcons :height="'14px'"
-                                 :iconstyle="iconstyle"
-                                 :icon="socialLink.network"/>
-                </div>
-                <div class="mx-auto text-center text-2xs text-neutral-500"> v-if=socialLink.followers
-                    {{ formatCount(socialLink.followers) }}
-                </div>
-            </div>
+  <div>
+    <!--sse-->
+    <div
+      v-if="iconstyle == 'vertical'"
+      class="mt-1 grid grid-cols-6 items-center text-center">
+      <div
+        v-for="socialLink in socialLinks"
+        :key="socialLink.id"
+        @click="openLink(socialLink.url, true)"
+        class="mx-auto cursor-pointer items-center justify-center">
+        <div class="text-neutral-5 mx-auto justify-center text-center">
+          <SocialIcons
+            :height="'14px'"
+            :iconstyle="iconstyle"
+            :icon="socialLink.network" />
         </div>
-        <div
-            v-if="iconstyle == 'horizontal'"
-            class="mx-auto mt-2 flex justify-start space-x-6">
-            <div v-for="socialLink in socialLinks"
-                 :key="socialLink.id" @click="openLink(socialLink.url, true)"
-                 class="flex cursor-pointer">
-                <SocialIcons :height="'14px'"
-                             :iconstyle="iconstyle"
-                             :icon="socialLink.network"/>
-                <span class="text-xs text-neutral-500" v-if="socialLink.followers">{{ formatCount(socialLink.followers) }}</span>
-            </div>
+        <div class="mx-auto text-center text-2xs text-neutral-500">
+          v-if=socialLink.followers
+          {{ formatCount(socialLink.followers) }}
         </div>
+      </div>
     </div>
+    <div
+      v-if="iconstyle == 'horizontal'"
+      class="mx-auto mt-2 flex justify-start space-x-6">
+      <div
+        v-for="socialLink in socialLinks"
+        :key="socialLink.id"
+        @click="openLink(socialLink.url, true)"
+        class="flex cursor-pointer">
+        <SocialIcons
+          :height="'14px'"
+          :iconstyle="iconstyle"
+          :icon="socialLink.network" />
+        <span class="text-xs text-neutral-500" v-if="socialLink.followers">{{
+          formatCount(socialLink.followers)
+        }}</span>
+      </div>
+    </div>
+    <!--/sse-->
+  </div>
 </template>
 <script>
-import SocialIcons from "../SocialIcons";
+import SocialIcons from '../SocialIcons';
 
 export default {
-    components: {SocialIcons},
-    props: {
-        iconstyle: {
-            type: String,
-            default: 'horizontal',
-        },
-        socialLinks: {
-            type: Array
-        }
+  components: { SocialIcons },
+  props: {
+    iconstyle: {
+      type: String,
+      default: 'horizontal',
     },
-    data() {
-        return {};
+    socialLinks: {
+      type: Array,
     },
+  },
+  data() {
+    return {};
+  },
 };
 </script>
