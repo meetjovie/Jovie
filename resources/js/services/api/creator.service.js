c25ee1b5badf63b15d356623f1fad829b9e817dc
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-03-20 23:24:40 -0400

    plan

--- resources/js/services/api/creator.service.js
+++ resources/js/services/api/creator.service.js
@@ -1,25 +1,28 @@
-
 const baseApiUrl = '/api';
 const baseUrlWeb = '';
 const baseUrlAdmin = '/api/admin';
 
 export default {
-    getComments(creatorId, limit) {
-        return axios.get(`${baseUrlAdmin}/get-comments/${creatorId}?limit=${limit}`);
-    },
-    async addComment(data) {
-        return axios.post(`${baseUrlAdmin}/add-comment`, data);
-    },
-    async previousCreator(id) {
-        return axios.get(`${baseUrlAdmin}/previous-creator/${id}`);
-    },
-    async nextCreator(id) {
-        return axios.get(`${baseUrlAdmin}/next-creator/${id}`);
-    },
-    async updateOverviewCreator(data) {
-        const id = data.id;
-        delete data.id;
-        return axios.post(
-            `${baseUrlAdmin}/update-overview-creator/${id}?_method=PUT`, data);
-    },
-}
+  getComments(creatorId, limit) {
+    return axios.get(
+      `${baseUrlAdmin}/get-comments/${creatorId}?limit=${limit}`
+    );
+  },
+  async addComment(data) {
+    return axios.post(`${baseUrlAdmin}/add-comment`, data);
+  },
+  async previousCreator(id) {
+    return axios.get(`${baseUrlAdmin}/previous-creator/${id}`);
+  },
+  async nextCreator(id) {
+    return axios.get(`${baseUrlAdmin}/next-creator/${id}`);
+  },
+  async updateOverviewCreator(data) {
+    const id = data.id;
+    delete data.id;
+    return axios.post(
+      `${baseUrlAdmin}/update-overview-creator/${id}?_method=PUT`,
+      data
+    );
+  },
+};
