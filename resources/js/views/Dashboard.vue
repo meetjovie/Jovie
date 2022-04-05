90b9638ec8f164bba66fe69027a0a3a38352fca8
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-04-05 15:39:01 -0400

    banner

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <AlertBanner
+      title="Your subscribtion is inactive"
+      subtitle="Please update your billing information to continue using Jovie."
+      cta="Update billing information"
+      ctaLink="/billing" />
     <div class="bg-white shadow">
       <div class="px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
         <div
@@ -128,6 +133,7 @@
 </template>
 
 <script>
+import AlertBanner from '../components/AlertBanner.vue';
 import TeamService from '../services/api/team.service';
 import { SearchIcon, CloudUploadIcon } from '@heroicons/vue/solid';
 import store from '../store/index';
@@ -155,6 +161,7 @@ export default {
   components: {
     SearchIcon,
     CloudUploadIcon,
+    AlertBanner,
   },
   data() {
     return {
