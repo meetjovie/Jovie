640797a6f2d122c57d30358ed31348e77e448e5d
Aamish Irfan <aamishirfan2662@gmail.com>
2022-04-21 00:28:19 +0500

    bactch size

--- config/scout.php
+++ config/scout.php
@@ -68,8 +68,8 @@ return [
     */
 
     'chunk' => [
-        'searchable' => 500,
-        'unsearchable' => 500,
+        'searchable' => 50000,
+        'unsearchable' => 50000,
     ],
 
     /*
