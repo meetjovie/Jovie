b6b2681a847b3814c7847ea0ecc93d6f00bf5417
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-04-29 21:15:09 -0400

    design

--- resources/js/views/Import.vue
+++ resources/js/views/Import.vue
@@ -170,62 +170,57 @@
               </p>
             </div>
           </div> -->
-          <div class="min-h-screen items-center py-4">
-            <draggable>
-              <template item="#">
-                <label class="sr-only block text-sm font-medium text-gray-700">
-                  Upload list
-                </label>
-                <div
-                  class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                  <div class="space-y-1 text-center">
-                    <svg
-                      class="mx-auto h-12 w-12 text-gray-400"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 48 48"
-                      aria-hidden="true">
-                      <path
-                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round" />
-                    </svg>
-                    <div class="flex text-sm text-gray-600">
-                      <label
-                        for="file-upload"
-                        class="focus-active:underline-indigo-500 focus-active:ring-offset-2 focus-active:outline-none focus-active:ring-2 relative cursor-pointer rounded-md bg-neutral-100 font-medium text-indigo-600 hover:text-indigo-500">
-                        <span>Upload a file</span>
-                        <input
-                          id="file-upload"
-                          name="file-upload"
-                          ref="file_upload"
-                          type="file"
-                          @change="getColumnsFromCsv()"
-                          class="sr-only" />
-                      </label>
-                      <p class="pl-1">or drag and drop</p>
-                    </div>
-                    <p class="text-xs text-gray-500">CSV</p>
-                  </div>
-                </div>
+          <div class="min-h-screen items-center py-12">
+            <label class="sr-only block text-sm font-medium text-gray-700">
+              Upload list
+            </label>
 
-                <ProgressBar v-if="uploadProgress" :percentage="uploadProgress">
-                  <p
-                    v-if="uploadProgress"
-                    class="middle-0 absolute mx-auto w-full text-center text-[8px] font-bold transition-all"
-                    :class="[
-                      { 'text-white': uploadProgress > 50 },
-                      { 'text-indigo-700': uploadProgress <= 50 },
-                    ]">
-                    {{ uploadProgress }}%
-                  </p>
-                </ProgressBar>
-                <p v-if="errors.key" class="mt-2 text-sm text-red-600">
-                  {{ errors.key[0] }}
-                </p>
-              </template>
-            </draggable>
+            <div
+              @dragcenter.prevent="toggelActive"
+              @dragleave.prevent="toggleActive"
+              @dragover.prevent
+              @drop.prevent="toggleActive"
+              :class="{ 'bg-indigo-100': active }"
+              class="group mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 py-12 hover:border-gray-400">
+              <div class="space-y-1 text-center">
+                <CloudUploadIcon
+                  class="mx-auto h-12 w-12 text-neutral-200 group-hover:text-gray-400" />
+                <div class="flex text-sm text-gray-600">
+                  <label
+                    for="file-upload"
+                    class="focus-active:underline-indigo-500 focus-active:ring-offset-2 focus-active:outline-none focus-active:ring-2 relative cursor-pointer rounded-md bg-neutral-100 font-medium text-indigo-600 hover:text-indigo-500">
+                    <span>Upload a file</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      ref="file_upload"
+                      type="file"
+                      @change="getColumnsFromCsv()"
+                      class="sr-only" />
+                  </label>
+                  <p class="pl-1">or drag and drop</p>
+                </div>
+                <p class="text-xs text-gray-500">CSV</p>
+              </div>
+            </div>
+
+            <ProgressBar
+              class="mt-4"
+              v-if="uploadProgress"
+              :percentage="uploadProgress">
+              <p
+                v-if="uploadProgress"
+                class="middle-0 absolute mx-auto w-full text-center text-[8px] font-bold transition-all"
+                :class="[
+                  { 'text-white': uploadProgress > 50 },
+                  { 'text-indigo-700': uploadProgress <= 50 },
+                ]">
+                {{ uploadProgress }}%
+              </p>
+            </ProgressBar>
+            <p v-if="errors.key" class="mt-2 text-sm text-red-600">
+              {{ errors.key[0] }}
+            </p>
           </div>
           <!--  <div class="flex justify-end">
             <button
@@ -260,6 +255,7 @@ import ImportService from '../services/api/import.service';
 import UserService from '../services/api/user.service';
 import ProgressBar from '../components/ProgressBar.vue';
 import draggable from 'vuedraggable';
+import { CloudUploadIcon } from '@heroicons/vue/solid';
 
 export default {
   name: 'Import',
@@ -271,6 +267,7 @@ export default {
     ImportColumnMatching,
     ProgressBar,
     draggable,
+    CloudUploadIcon,
   },
   data() {
     return {
@@ -278,6 +275,7 @@ export default {
       showMapping: false,
       columns: [],
       errors: [],
+      active: false,
       drag: false,
       importSet: {
         instagram: null,
@@ -295,6 +293,9 @@ export default {
     this.getUserLists();
   },
   methods: {
+    toggleActive() {
+      this.active = !this.active;
+    },
     getUserLists() {
       UserService.getUserLists().then((response) => {
         response = response.data;
