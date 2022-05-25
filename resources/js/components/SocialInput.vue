4db374d0cec945cd82341ab4a6dd43f02f4162bb
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-05-25 16:33:30 -0400

    input

--- resources/js/components/SocialInput.vue
+++ resources/js/components/SocialInput.vue
@@ -30,6 +30,12 @@
           height="20"
           width="20" />
         <SocialIcons
+          v-else-if="socialMediaProfileUrl.includes('snapchat')"
+          icon="snapchat"
+          height="20"
+          width="20" />
+
+        <SocialIcons
           v-else-if="socialMediaProfileUrl.includes('tiktok')"
           icon="tiktok"
           height="20"
@@ -45,20 +51,28 @@
         type="social-media-profile-url"
         name="social-media-profile-url"
         id="social-media-profile-url"
-        class="block w-full rounded-md border-gray-300 py-2 pl-10 placeholder:font-semibold placeholder:text-neutral-400 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        class="border-1 block w-full rounded-md border-indigo-300 py-2 pl-10 placeholder:font-semibold placeholder:text-neutral-400 focus:border-indigo-500 focus:ring-indigo-500 active:border-indigo-500 sm:text-sm"
         placeholder="http://instagram.com/username" />
+      <div
+        class="group absolute inset-y-0 right-0 flex cursor-pointer py-1.5 pr-3 active:pr-2.5">
+        <kbd
+          class="inline-flex select-none items-center rounded border border-indigo-200 px-2 font-sans text-sm font-medium text-indigo-400 active:border-indigo-500 group-hover:border-indigo-400">
+          Import
+        </kbd>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { UserIcon } from '@heroicons/vue/solid';
+import { UserIcon, ClipboardIcon } from '@heroicons/vue/solid';
 import SocialIcons from './SocialIcons';
 
 export default {
   components: {
     UserIcon,
     SocialIcons,
+    ClipboardIcon,
   },
 
   props: {
