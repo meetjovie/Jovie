03c96b338d64614d2ef6a68c47d3a98934e99f3c
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-05-27 13:31:50 -0400

    a

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -151,7 +151,6 @@
           <div class="z-10 flex items-center">
             <div class="inline-flex items-center space-x-4">
               <div
-                v-if=""
                 as="router-link"
                 to="/account"
                 class="underline-2 cursor-pointer text-xs font-bold text-indigo-500 decoration-indigo-700 hover:underline">
