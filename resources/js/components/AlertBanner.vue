0eee7b7db4761a707838f80bcec8fb05e27c1434
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-06-01 12:55:35 -0400

    dismissbanner

--- resources/js/components/AlertBanner.vue
+++ resources/js/components/AlertBanner.vue
@@ -1,11 +1,11 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <div
+    v-if="!dismissed"
     class="relative"
     :class="[
       { 'bg-indigo-500': design == 'primary' },
       { 'bg-red-500 ': design == 'danger' },
-      { 'hidden transition-all': dismissed },
     ]">
     <div class="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
       <div class="pr-16 sm:px-16 sm:text-center">
@@ -26,12 +26,12 @@
       <div
         class="absolute inset-y-0 right-0 flex items-start pt-1 pr-1 sm:items-start sm:pt-1 sm:pr-2">
         <button
-          @click="dismissed"
+          @click="dismissed = true"
           type="button"
           class="group flex rounded-md p-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white">
           <span class="sr-only">Dismiss</span>
           <XIcon
-            class="h-6 w-6 font-medium text-neutral-50 group-hover:font-bold group-hover:text-white"
+            class="active:text-neutal-100 h-6 w-6 rounded-full p-1 font-medium text-neutral-50 transition duration-300 ease-in-out active:bg-neutral-100/30 active:text-neutral-400 group-hover:font-bold group-hover:text-white"
             aria-hidden="true" />
         </button>
       </div>
@@ -43,6 +43,11 @@
 import { XIcon } from '@heroicons/vue/outline';
 
 export default {
+  data() {
+    return {
+      dismissed: false,
+    };
+  },
   props: {
     design: {
       type: String,
