8c2a926c5c01f57676b2e1ee19157c7c5ff68a83
Aamish Irfan <aamishirfan2662@gmail.com>
2022-07-07 00:59:26 +0500

    twitch fix

--- app/Traits/SocialScrapperTrait.php
+++ app/Traits/SocialScrapperTrait.php
@@ -79,12 +79,27 @@ trait SocialScrapperTrait {
     {
         try {
 
-            $client = new Client();
-            $request = new Request('GET', 'https://twitchtracker.com/api/channels/summary/mrsruvi');
-            $response = $client->sendAsync($request)->wait();
-            return $response;
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => ('https://twitchtracker.com/api/channels/summary/'.$username),
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            Log::channel('slack_warning')->info(json_encode($response));
+            return ($response);
+
         } catch (\Exception $e) {
-            return $e->getResponse();
+            return null;
         }
     }
 
