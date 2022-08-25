319aa8c8f4938b0e89db5d5180ed624f47024438
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-08-25 15:56:48 -0700

    enrichment

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -112,7 +112,7 @@ import HomeTestimonials from '../components/Home/HomeTestimonials';
 import HomeCTA2 from '../components/Home/HomeCTA2';
 import HomeFeatureCRM from '../components/Home/HomeFeatureCRM';
 import HomeCreatorSearch from '../components/Home/HomeCreatorSearch';
-import Enrichment from '../components/Home/Enrichment';
+import Enrichment from '../components/Enrichment';
 
 import CallToAction from '../components/CallToAction';
 
