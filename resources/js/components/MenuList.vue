4e06190df96c5fad9248ab582bc4d543684f3861
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-08-27 16:27:18 -0700

    cleans up list design

--- resources/js/components/MenuList.vue
+++ resources/js/components/MenuList.vue
@@ -24,13 +24,14 @@
         <div
           class="inline-flex w-full items-center justify-between rounded-md transition-all hover:bg-neutral-200">
           <div
-            class="group w-6 flex-none cursor-grab items-center hover:bg-neutral-200">
+            class="group w-4 flex-none cursor-grab items-center hover:bg-neutral-200">
             <MenuIcon
               class="h-4 w-4 text-gray-400/0 group-hover:text-gray-400"></MenuIcon>
           </div>
 
           <div class="flex w-full">
-            <div class="rounded-md p-1 text-xs hover:bg-gray-400">
+            <div
+              class="cursor-pointer rounded-md p-1 text-xs hover:bg-gray-400">
               {{ item.emoji || '😆' }}
             </div>
             <div>
@@ -49,7 +50,7 @@
             </div>
           </div>
           <div
-            class="w-6 flex-none rounded-md p-1 hover:bg-gray-500 hover:text-gray-50">
+            class="w-6 flex-none cursor-pointer rounded-md p-1 hover:bg-gray-300 hover:text-gray-50">
             <span
               class="text-xs font-semibold text-neutral-400 group-hover:hidden group-hover:text-neutral-500"
               >{{ item.count }}</span
