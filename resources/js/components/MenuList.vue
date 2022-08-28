d6fe9d36e64617e287dd79be2538a273402880bf
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-08-27 18:00:22 -0700

    Addds dropdown menu

--- resources/js/components/MenuList.vue
+++ resources/js/components/MenuList.vue
@@ -55,8 +55,73 @@
               class="items-center text-xs font-semibold text-neutral-400 group-hover:hidden group-hover:text-neutral-500"
               >{{ item.count }}</span
             >
-            <DotsVerticalIcon
-              class="hidden h-4 w-4 text-gray-400 hover:text-gray-50 group-hover:block"></DotsVerticalIcon>
+
+            <Menu as="div" class="relative inline-block text-left">
+              <div>
+                <MenuButton
+                  class="hidden h-4 w-4 text-gray-400 hover:text-gray-50 group-hover:block">
+                  <DotsVerticalIcon
+                    class="hidden h-4 w-4 text-gray-400 hover:text-gray-50 group-hover:block"></DotsVerticalIcon>
+                </MenuButton>
+              </div>
+
+              <transition
+                enter-active-class="transition duration-100 ease-out"
+                enter-from-class="transform scale-95 opacity-0"
+                enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-75 ease-in"
+                leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0">
+                <MenuItems
+                  class="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md border-neutral-200 bg-white/60 shadow-lg ring-1 ring-black ring-opacity-5 backdrop-blur-2xl backdrop-saturate-150 focus:outline-none">
+                  <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                      <button
+                        :class="[
+                          active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                          'group flex w-full items-center rounded-md px-2 py-2 text-xs',
+                        ]">
+                        <DuplicateIcon
+                          :active="active"
+                          class="mr-2 h-5 w-5 text-violet-400"
+                          aria-hidden="true" />
+                        Duplicate
+                      </button>
+                    </MenuItem>
+
+                    <MenuItem v-slot="{ active }">
+                      <button
+                        :class="[
+                          active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                          'group flex w-full items-center rounded-md px-2 py-2 text-xs',
+                        ]">
+                        <ArchiveIcon
+                          :active="active"
+                          class="mr-2 h-5 w-5 text-violet-400"
+                          aria-hidden="true" />
+                        Archive
+                      </button>
+                    </MenuItem>
+                  </div>
+
+                  <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                      <button
+                        :class="[
+                          active ? 'bg-violet-500 text-white' : 'text-gray-900',
+                          'group flex w-full items-center rounded-md px-2 py-2 text-xs',
+                        ]">
+                        <DeleteIcon
+                          :active="active"
+                          class="mr-2 h-5 w-5 text-violet-400"
+                          aria-hidden="true" />
+                        Delete
+                      </button>
+                    </MenuItem>
+                  </div>
+                </MenuItems>
+              </transition>
+            </Menu>
           </div>
         </div>
       </div>
@@ -70,7 +135,13 @@ import {
   PlusIcon,
   MenuIcon,
   DotsVerticalIcon,
+  EditIcon,
+  DuplicateIcon,
+  ArchiveIcon,
+  MoveIcon,
+  DeleteIcon,
 } from '@heroicons/vue/solid';
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
 
 export default {
   data() {
@@ -96,6 +167,15 @@ export default {
     DotsVerticalIcon,
     PlusIcon,
     MenuIcon,
+    ArchiveIcon,
+    DuplicateIcon,
+    MoveIcon,
+    EditIcon,
+    DeleteIcon,
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem,
   },
   props: {
     menuName: {
