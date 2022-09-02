e53051843240ac5f1e8cb1654abba31b2790b1ea
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-09-01 23:38:36 -0700

    a

--- resources/js/views/Crm.vue
+++ resources/js/views/Crm.vue
@@ -15,34 +15,34 @@
             :class="[{ '-mt-20': $store.state.CRMSidebarOpen }, '-mt-10']">
             <div>
               <div class="mt-10 flex-col py-1 px-2">
-                <JovieTooltip>
-                  <button
-                    class="group flex h-6 w-full items-center justify-between rounded-md text-left hover:bg-neutral-200 hover:text-neutral-500"
-                    :class="[
-                      selected
-                        ? 'text-sm font-bold text-neutral-500  '
-                        : 'text-sm font-semibold text-neutral-400',
-                    ]">
-                    <div class="flex items-center text-xs">
-                      <UserGroupIcon
-                        class="mr-1 h-5 w-5 rounded-md p-1 text-pink-400"
-                        aria-hidden="true" />
-                      All Contacts
-                    </div>
-                    <div
-                      @click="showCreatorModal = true"
-                      class="items-center rounded-md p-1 hover:bg-gray-300 hover:text-gray-50">
-                      <span
-                        class="text-xs font-semibold text-neutral-400 group-hover:hidden group-hover:text-neutral-500"
-                        >{{ creators.length }}</span
-                      >
+                <!--   <JovieTooltip> -->
+                <button
+                  class="group flex h-6 w-full items-center justify-between rounded-md text-left hover:bg-neutral-200 hover:text-neutral-500"
+                  :class="[
+                    selected
+                      ? 'text-sm font-bold text-neutral-500  '
+                      : 'text-sm font-semibold text-neutral-400',
+                  ]">
+                  <div class="flex items-center text-xs">
+                    <UserGroupIcon
+                      class="mr-1 h-5 w-5 rounded-md p-1 text-pink-400"
+                      aria-hidden="true" />
+                    All Contacts
+                  </div>
+                  <div
+                    @click="showCreatorModal = true"
+                    class="items-center rounded-md p-1 hover:bg-gray-300 hover:text-gray-50">
+                    <span
+                      class="text-xs font-semibold text-neutral-400 group-hover:hidden group-hover:text-neutral-500"
+                      >{{ creators.length }}</span
+                    >
 
-                      <PlusIcon
-                        class="hidden h-3 w-3 text-gray-400 active:text-white group-hover:block"></PlusIcon>
-                    </div>
-                  </button>
-                </JovieTooltip>
-
+                    <PlusIcon
+                      class="hidden h-3 w-3 text-gray-400 active:text-white group-hover:block"></PlusIcon>
+                  </div>
+                </button>
+                <!--   </JovieTooltip>
+ -->
                 <button
                   class="group flex h-6 w-full items-center justify-between rounded-md py-1 text-left hover:bg-neutral-200 hover:text-neutral-500"
                   :class="[
