82b1a789aeb38b5cf807b0817f7482b849127ebb
Aamish Irfan <aamishirfan2662@gmail.com>
2022-09-06 08:52:15 +0500

    emoji picker fix

--- resources/js/views/Crm.vue
+++ resources/js/views/Crm.vue
@@ -88,24 +88,19 @@
               </div>
               <div class="flex-col justify-evenly space-y-4 px-2 py-4">
                 <MenuList
+                    ref="menuListPinned"
                   @getUserLists="getUserLists"
+                  @openEmojiPicker="openEmojiPicker"
                   menuName="Pinned"
                   :selectedList="filters.list"
                   @setFilterList="setFilterList"
                   :menuItems="pinnedUserLists"></MenuList>
                 <!--    Team Specific Lists -->
                 <MenuList
+                    ref="menuListAll"
                   @getUserLists="getUserLists"
+                  @openEmojiPicker="openEmojiPicker"
                   menuName="Teamspace"
-                  @setFilterList="setFilterList"
-                  :selectedList="filters.list"
-                  :draggable="true"
-                  @end="sortLists"
-                  :menuItems="filteredUsersLists"></MenuList>
-                <!--   User Specific Lists -->
-                <MenuList
-                  @getUserLists="getUserLists"
-                  menuName="Private"
                   @setFilterList="setFilterList"
                   :selectedList="filters.list"
                   :draggable="true"
@@ -233,6 +228,14 @@
       </div>
 
       <ImportCreatorModal :open="showCreatorModal" />
+
+
+
+    <EmojiPickerModal
+        v-show="openEmojis"
+        @emojiSelected="emojiSelected($event)"
+        class="w-4 cursor-pointer items-center rounded-md bg-gray-50 text-center text-xs hover:bg-neutral-400 group-hover:bg-neutral-200">
+    </EmojiPickerModal>
     </div>
   </div>
 </template>
@@ -276,10 +279,12 @@ import MenuList from '../components/MenuList';
 import ProgressBar from '../components/ProgressBar';
 import SwitchTeams from '../components/SwitchTeams';
 import JovieTooltip from '../components/JovieTooltip.vue';
+import EmojiPickerModal from '../components/EmojiPickerModal.vue';
 
 export default {
   name: 'CRM',
   components: {
+      EmojiPickerModal,
     CloudArrowDownIcon,
     PlusIcon,
     SwitchTeams,
@@ -363,6 +368,8 @@ export default {
       },
       searchList: '',
       abortController: null,
+        openEmojis: false,
+        selectedList: null
     };
   },
   watch: {
@@ -402,6 +409,17 @@ export default {
     this.getCrmCreators();
   },
   methods: {
+      openEmojiPicker(item) {
+          this.selectedList = item
+          this.openEmojis = true
+      },
+      emojiSelected(emoji) {
+          //take the value of the selected emoji and set it to the emoji variable
+          this.selectedList.emoji = emoji.i;
+          this.$refs.menuListAll.updateList(JSON.parse(JSON.stringify(this.selectedList)))
+          this.selectedList = null
+          this.openEmojis = false
+      },
     setFiltersType(type) {
       this.filters.type = this.filters.type == type ? 'all' : type;
       this.filters.list = null;
