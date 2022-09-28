0a53bf4efab68790e8cb6654b66607846b922a34
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-09-27 22:44:54 -0700

    tracking

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -161,6 +161,8 @@ export default {
             this.successfulLogin = true;
             window.analytics.track('User Logged In', {
               email: this.user.email,
+              first_name: this.user.first_name,
+              last_name: this.user.last_name,
             });
             router.push({ name: 'Contacts' });
           } else {
