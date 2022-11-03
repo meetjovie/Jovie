30466f745096559428ce58f2d273189b1b6019bc
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-11-03 10:42:55 -0700

    Brand

---
+++ resources/js/components/MarketingAsset.vue
@@ -0,0 +1,84 @@
+<template>
+  <section>
+    <div class="grid place-items-center bg-gray-700 p-4">
+      <div>
+        <div
+          :id="'Jovie_MA_' + divId"
+          :class="`h-[${assetHeight}px] w-[${assetWidth}px]`"
+          class="relative overflow-hidden bg-gradient-to-b from-neutral-200 to-neutral-50">
+          <slot>
+            <!-- Content -->
+            <div
+              class="relative mt-24 flex h-full w-full flex-col items-center space-y-4 p-8 text-center">
+              <h1
+                class="bg-gradient-to-r from-sky-500 via-blue-600 to-fuchsia-500 bg-clip-text text-2xl font-semibold uppercase tracking-wide text-transparent">
+                Meet Jovie
+              </h1>
+
+              <h2
+                class="uppercse text-7xl font-extrabold tracking-normal text-neutral-800 md:text-5xl xl:text-8xl">
+                The Social CRM
+                <br />
+              </h2>
+              <h2
+                class="mb-12 inline py-2 text-center text-xl font-bold text-neutral-500">
+                A
+                <span
+                  class="dexoration-2 font-bold underline decoration-pink-400"
+                  >smarter</span
+                >
+                way to organize everyone you follow<br />
+              </h2>
+
+              <img
+                class="rounded-lg shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5"
+                :src="asset('img/External/HomeFeatureCRM.webp')" />
+            </div>
+          </slot>
+        </div>
+      </div>
+      <div class="flex w-full max-w-5xl items-center justify-between py-4">
+        <h2 class="text-md font-semibold text-white">{{ name }}</h2>
+
+        <ButtonGroup
+          class="w-60"
+          text="Download"
+          @click="downloadImage('Jovie_MA_' + divId)">
+          Download
+        </ButtonGroup>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import ButtonGroup from '../components/ButtonGroup.vue';
+export default {
+  components: {
+    ButtonGroup,
+  },
+  props: {
+    name: {
+      type: String,
+      default: 'Marquee promo tile',
+    },
+    divId: {
+      type: String,
+      default: 'MarqueePromoTile',
+    },
+    assetHeight: {
+      type: Number,
+      default: 560,
+    },
+    assetWidth: {
+      type: Number,
+      default: 1400,
+    },
+  },
+  methods: {
+    downloadImage(idFromParent) {
+      this.$emit('downloadImage', idFromParent);
+      console.log('downloadImage', idFromParent);
+    },
+  },
+};
+</script>
--- resources/js/views/MarketingAssets.vue
+++ resources/js/views/MarketingAssets.vue
@@ -6,146 +6,118 @@
         Download All
       </ButtonGroup>
     </div>
-    <section>
-      <div class="grid place-items-center bg-gray-700 p-4">
-        <div
-          id="ChromeBanner"
-          class="relative h-[560px] w-[1400px] overflow-hidden bg-gradient-to-b from-neutral-200 to-neutral-50">
-          <!-- Content -->
-          <div
-            class="relative mt-24 flex h-full w-full flex-col items-center space-y-4 p-8 text-center">
-            <h1
-              class="bg-gradient-to-r from-sky-500 via-blue-600 to-fuchsia-500 bg-clip-text text-2xl font-semibold uppercase tracking-wide text-transparent">
-              Meet Jovie
-            </h1>
+    <MarketingAsset
+      @downloadImage="downloadImage($event)"
+      :assetHeight="560"
+      :assetWidth="1400"
+      :divId="'MarqueePromoTile'"
+      :name="'Marquee Promo Tile'">
+      <!-- Content -->
+      <div
+        class="relative mt-24 flex h-full w-full flex-col items-center space-y-4 p-8 text-center">
+        <h1
+          class="bg-gradient-to-r from-sky-500 via-blue-600 to-fuchsia-500 bg-clip-text text-2xl font-semibold uppercase tracking-wide text-transparent">
+          Meet Jovie
+        </h1>
 
-            <h2
-              class="uppercse text-7xl font-extrabold tracking-normal text-neutral-800 md:text-5xl xl:text-8xl">
-              The Social CRM
-              <br />
-            </h2>
-            <h2
-              class="inline py-2 text-center text-xl font-bold text-neutral-500">
-              A
-              <span class="dexoration-2 font-bold underline decoration-pink-400"
-                >smarter</span
-              >
-              way to organize everyone you follow<br />
-            </h2>
-          </div>
-          <div class="hidden max-w-7xl sm:block">
-            <img
-              class="rounded-lg shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5"
-              :src="asset('img/External/HomeFeatureCRM.webp')" />
-          </div>
+        <h2
+          class="uppercse text-7xl font-extrabold tracking-normal text-neutral-800 md:text-5xl xl:text-8xl">
+          The Social CRM
+          <br />
+        </h2>
+        <h2
+          class="mb-12 inline py-2 text-center text-xl font-bold text-neutral-500">
+          A
+          <span class="dexoration-2 font-bold underline decoration-pink-400"
+            >smarter</span
+          >
+          way to organize everyone you follow<br />
+        </h2>
+
+        <img
+          class="rounded-lg shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5"
+          :src="asset('img/External/HomeFeatureCRM.webp')" />
+      </div>
+    </MarketingAsset>
+    <MarketingAsset
+      @downloadImage="downloadImage($event)"
+      :divId="'ChromeSS1'"
+      :assetHeight="800"
+      :assetWidth="1280"
+      :name="'Chrome Screenshot'">
+      <!-- Content -->
+      <div
+        class="relative mt-24 flex h-full w-full flex-col items-center space-y-4 p-8 text-center">
+        <h1
+          class="bg-gradient-to-r from-sky-500 via-blue-600 to-fuchsia-500 bg-clip-text text-4xl font-semibold uppercase tracking-wide text-transparent">
+          Meet Jovie
+        </h1>
+
+        <h2
+          class="uppercse text-7xl font-extrabold tracking-normal text-neutral-800 md:text-5xl xl:text-8xl">
+          The Social CRM
+          <br />
+        </h2>
+        <h2 class="inline py-2 text-center text-3xl font-bold text-neutral-500">
+          A
+          <span class="dexoration-2 font-bold underline decoration-pink-400"
+            >smarter</span
+          >
+          way to organize everyone you follow<br />
+        </h2>
+        <img
+          class="mt-48 rounded-lg shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5"
+          :src="asset('img/External/HomeFeatureCRM.webp')" />
+      </div>
+    </MarketingAsset>
+    <MarketingAsset
+      @downloadImage="downloadImage($event)"
+      :divId="'ChromeSS2'"
+      :assetHeight="800"
+      :assetWidth="1280"
+      :name="'Chrome Screenshot'">
+      <!-- Content -->
+      <div class="relative grid h-full w-full grid-cols-2 p-8">
+        <div class="mt-80 items-center text-center">
+          <h1
+            class="mt-6 items-center text-3xl font-semibold uppercase tracking-wide text-sky-500">
+            Save Anyone In A Click
+          </h1>
         </div>
-        <div class="flex w-full max-w-5xl items-center justify-between py-4">
-          <h2 class="text-md font-semibold text-white">
-            Chrome Web Store Banner
-          </h2>
-
-          <ButtonGroup
-            class="w-60"
-            text="Download"
-            @click="downloadImage('ChromeBanner')">
-            Download
-          </ButtonGroup>
+        <div>
+          <img
+            class="rounded-xl shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5"
+            :src="
+              asset(
+                'img/External/Marketing/Chrome/Jovie_Chrome_Extension_Screenshot.png'
+              )
+            " />
         </div>
       </div>
-    </section>
-    <section>
-      <div class="grid place-items-center bg-gray-700 p-4">
-        <div
-          id="ChromeSS1"
-          class="relative h-[400px] w-[640px] overflow-hidden bg-gradient-to-b from-neutral-200 to-neutral-50">
-          <!-- Content -->
-          <div
-            class="relative mt-24 flex h-full w-full flex-col items-center space-y-4 p-8 text-center">
-            <h1
-              class="bg-gradient-to-r from-sky-500 via-blue-600 to-fuchsia-500 bg-clip-text text-2xl font-semibold uppercase tracking-wide text-transparent">
-              Meet Jovie
-            </h1>
-
-            <h2
-              class="uppercse text-7xl font-extrabold tracking-normal text-neutral-800 md:text-5xl xl:text-8xl">
-              The Social CRM
-              <br />
-            </h2>
-            <h2
-              class="inline py-2 text-center text-xl font-bold text-neutral-500">
-              A
-              <span class="dexoration-2 font-bold underline decoration-pink-400"
-                >smarter</span
-              >
-              way to organize everyone you follow<br />
-            </h2>
-          </div>
-          <div class="hidden max-w-7xl sm:block">
-            <img
-              class="rounded-lg shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5"
-              :src="asset('img/External/HomeFeatureCRM.webp')" />
-          </div>
-        </div>
-
-        <div class="flex w-full max-w-5xl items-center justify-between py-4">
-          <h2 class="text-md font-semibold text-white">Chrome Screenshot</h2>
-
-          <ButtonGroup
-            class="w-60"
-            text="Download"
-            @click="downloadImage('ChromeSS1')">
-            Download
-          </ButtonGroup>
-        </div>
-      </div>
-    </section>
-    <section>
-      <div class="grid place-items-center bg-gray-700 p-4">
-        <div
-          id="ChromeSS2"
-          class="relative h-[400px] w-[640px] overflow-hidden bg-gradient-to-b from-neutral-200 to-neutral-50">
-          <!-- Content -->
-          <div
-            class="relative mt-24 flex h-full w-full flex-col items-center space-y-4 p-8">
-            <h1
-              class="text-md font-semibold uppercase tracking-wide text-sky-500 text-transparent">
-              Save Anyone In A Click
-            </h1>
-          </div>
-          <div class="hidden max-w-7xl sm:block">
-            <img
-              class="rounded-lg shadow-xl shadow-indigo-700/30 ring-1 ring-black ring-opacity-5"
-              :src="asset('img/External/HomeFeatureCRM.webp')" />
-          </div>
-        </div>
-
-        <div class="flex w-full max-w-5xl items-center justify-between py-4">
-          <h2 class="text-md font-semibold text-white">Chrome Screenshot 2</h2>
-
-          <ButtonGroup
-            class="w-60"
-            text="Download"
-            @click="downloadImage('ChromeSS2')">
-            Download
-          </ButtonGroup>
-        </div>
-      </div>
-    </section>
+    </MarketingAsset>
   </div>
 </template>
 <script>
 import JovieLogo from './../components/JovieLogo.vue';
 import ButtonGroup from './../components/ButtonGroup.vue';
 import * as html2canvas from 'html2canvas';
+import MarketingAsset from './../components/MarketingAsset.vue';
 
 export default {
   components: {
     JovieLogo,
     ButtonGroup,
+    MarketingAsset,
+  },
+  data() {
+    return {
+      divHeight: 0,
+    };
   },
   methods: {
     // Download image
     downloadImage(id) {
+      console.log(id);
       // Get the image from the id
       const image = document.getElementById(id);
       console.log(image);
@@ -165,7 +137,7 @@ export default {
     // Download all images
     downloadAll() {
       // Get all the images
-      const images = document.querySelectorAll('div[id^="Chrome"]');
+      const images = document.querySelectorAll('div[id^="Jovie_MA_"]');
       // Loop through the images
       images.forEach((image) => {
         // Convert the image to a canvas
