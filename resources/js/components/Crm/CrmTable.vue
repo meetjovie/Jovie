6006984fe9b105ec51263887aec820aee62826f2
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-11-06 15:49:31 -0800

    Fixes glitch in crm table

--- resources/js/components/Crm/CrmTable.vue
+++ resources/js/components/Crm/CrmTable.vue
@@ -579,7 +579,7 @@
                         </div>
                         <div
                           @click="$emit('openSidebar', creator)"
-                          class="mx-auto items-center rounded-full bg-neutral-200/0 p-1 text-neutral-400 hover:bg-neutral-200 active:border">
+                          class="mx-auto items-center w-3 rounded-full bg-neutral-200/0 p-1 text-neutral-400 hover:bg-neutral-200 active:border">
                           <ArrowsPointingOutIcon
                             class="hidden h-3 w-3 group-hover:block" />
                         </div>
