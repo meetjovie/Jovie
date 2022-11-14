5d4cae326e18d93e589238c08ddaebd35498ac12
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-11-14 10:20:53 -0800

    Make CRM count fonts more readable

--- resources/js/views/Crm.vue
+++ resources/js/views/Crm.vue
@@ -36,7 +36,7 @@
                       @click="showCreatorModal = true"
                       class="items-center rounded-md p-1 hover:bg-gray-300 hover:text-gray-50">
                       <span
-                        class="text-xs font-medium text-neutral-900 group-hover:hidden group-hover:text-neutral-900"
+                        class="text-xs font-light text-neutral-900 group-hover:hidden group-hover:text-neutral-900"
                         >{{ counts.total }}</span
                       >
 
@@ -67,7 +67,7 @@
                     </div>
                     <div class="items-center rounded-md p-1 hover:text-gray-50">
                       <span
-                        class="text-xs font-medium text-neutral-700 group-hover:text-neutral-900"
+                        class="text-xs font-light text-neutral-700 group-hover:text-neutral-900"
                         >{{ counts.archived }}</span
                       >
                     </div>
@@ -89,7 +89,7 @@
                     </div>
                     <div class="items-center rounded-md p-1 hover:text-gray-50">
                       <span
-                        class="text-xs font-medium text-neutral-700 group-hover:text-neutral-900"
+                        class="text-xs font-light text-neutral-700 group-hover:text-neutral-900"
                         >{{ counts.favourites }}</span
                       >
                     </div>
