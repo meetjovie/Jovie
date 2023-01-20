7d9fb721c210f199b06826cdd3f852e0a82f94e5
Aamish26622 <aamishirfan2662@gmail.com>
2023-01-20 12:46:44 +0500

    optisn

--- resources/js/components/CustomFieldsMenu.vue
+++ resources/js/components/CustomFieldsMenu.vue
@@ -21,50 +21,52 @@
               class="w-full border-0 border-none border-transparent bg-transparent px-1 py-2 text-xs font-medium text-slate-600 outline-0 ring-0 placeholder:font-light placeholder:text-slate-400 focus:border-transparent focus:ring-0 focus:ring-transparent focus:ring-offset-0" />
             <ComboboxMenu :items="customFieldTypes" v-model="field.type" />
           </div>
-          <div class="px-2 py-1" v-if="field.type">
+          <div class="px-2 py-1" v-if="field.type.description">
             <p
               class="text-2xs font-semibold text-slate-600 dark:text-jovieDark-300">
               {{ field.type.description }}
             </p>
           </div>
-          <div class="flex justify-end">
-            <ButtonGroup
-              text="Alphabatize Options"
-              @click="alphabatizeOptions"
-              design="secondary"
-              size="xs"
-              class="text-2xs font-medium text-white dark:text-jovieDark-200">
-              <BarsArrowUpIcon class="nl-2 h-4 w-4" />
-            </ButtonGroup>
-          </div>
-          <div class="border-t border-slate-200 dark:border-jovieDark-border">
-            <div>
-              <div class="flex flex-col space-y-4">
-                <div>
-                  <ul class="space-y-2 py-1 px-2">
-                    <template v-for="(option, index) in field.options">
-                      <li class="flex justify-between">
-                        <Bars2Icon
-                          class="h-5 w-5 text-slate-600 dark:text-jovieDark-200" />
-                        <input
-                            v-model="field.options[index]"
-                          placeholder="Option Name"
-                          class="inline-flex w-full items-center rounded-md bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800" />
-                        <XMarkIcon
-                          @click="removeOption(index)"
-                          class="h-5 w-5 cursor-pointer rounded-md border border-slate-300 p-0.5 hover:bg-slate-200 hover:bg-jovieDark-700 hover:text-white dark:border-jovieDark-border" />
-                      </li>
-                    </template>
-                    <div
-                      class="flex w-full cursor-pointer items-center rounded-md border border-slate-300 p-0.5 text-xs font-semibold hover:bg-slate-200 hover:bg-jovieDark-700 hover:text-white dark:border-jovieDark-border">
-                      <PlusIcon @click="addOption()" class="h-5 w-5" />
-                      Add an option
-                    </div>
-                  </ul>
+            <template v-if="['select', 'multi_select'].includes(field.type.id)" >
+                <div class="flex justify-end">
+                    <ButtonGroup
+                        text="Alphabatize Options"
+                        @click="alphabatizeOptions"
+                        design="secondary"
+                        size="xs"
+                        class="text-2xs font-medium text-white dark:text-jovieDark-200">
+                        <BarsArrowUpIcon class="nl-2 h-4 w-4" />
+                    </ButtonGroup>
                 </div>
-              </div>
-            </div>
-          </div>
+                <div class="border-t border-slate-200 dark:border-jovieDark-border">
+                    <div>
+                        <div class="flex flex-col space-y-4">
+                            <div>
+                                <ul class="space-y-2 py-1 px-2">
+                                    <template v-for="(option, index) in field.options">
+                                        <li class="flex justify-between">
+                                            <Bars2Icon
+                                                class="h-5 w-5 text-slate-600 dark:text-jovieDark-200" />
+                                            <input
+                                                v-model="field.options[index]"
+                                                placeholder="Option Name"
+                                                class="inline-flex w-full items-center rounded-md bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800" />
+                                            <XMarkIcon
+                                                @click="removeOption(index)"
+                                                class="h-5 w-5 cursor-pointer rounded-md border border-slate-300 p-0.5 hover:bg-slate-200 hover:bg-jovieDark-700 hover:text-white dark:border-jovieDark-border" />
+                                        </li>
+                                    </template>
+                                    <div
+                                        class="flex w-full cursor-pointer items-center rounded-md border border-slate-300 p-0.5 text-xs font-semibold hover:bg-slate-200 hover:bg-jovieDark-700 hover:text-white dark:border-jovieDark-border">
+                                        <PlusIcon @click="addOption()" class="h-5 w-5" />
+                                        Add an option
+                                    </div>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
           <div
             v-if="errors.type"
             class="min-h-4 text-xs text-red-600 dark:text-red-300">
