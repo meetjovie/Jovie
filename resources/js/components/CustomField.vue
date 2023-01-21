9bfdfdf8023a8fbd48de81878e7c259fb7237319
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-01-20 22:25:53 -0800

    a

--- resources/js/components/CustomField.vue
+++ resources/js/components/CustomField.vue
@@ -1,58 +1,73 @@
 <template>
-    <label v-if="name">{{ name }}</label>
-    <template v-if="type === 'text' || type === 'number' || type === 'url'">
-        <input :type="type" v-model="modelValue" @change="$emit('updateModelValue', $event.target.value)">
+  <label v-if="name">{{ name }}</label>
+  <template v-if="type === 'text' || type === 'number' || type === 'url'">
+    <DataInputGroup
+      :type="type"
+      v-model="modelValue"
+      @change="$emit('updateModelValue', $event.target.value)" />
+  </template>
+  <template v-if="type === 'currency'">
+    <!--        use datagrid cell for input currency design-->
+    <input
+      type="number"
+      v-model="modelValue"
+      @change="$emit('updateModelValue', $event.target.value)" />
+  </template>
+  <template v-if="type === 'checkbox'">
+    <template v-for="option in options">
+      <label :for="option.id">{{ option.name }}</label>
+      <input
+        :id="option.id"
+        type="checkbox"
+        :value="option.id"
+        :name="name"
+        @change="$emit('updateModelValue', $event.target.value)" />
     </template>
-    <template v-if="type === 'currency'">
-<!--        use datagrid cell for input currency design-->
-        <input type="number" v-model="modelValue" @change="$emit('updateModelValue', $event.target.value)">
-    </template>
-    <template v-if="type === 'checkbox'">
-        <template v-for="option in options">
-            <label :for="option.id">{{ option.name }}</label>
-            <input :id="option.id" type="checkbox" :value="option.id" :name="name" @change="$emit('updateModelValue', $event.target.value)">
-        </template>
-    </template>
-    <template v-else-if="type === 'date'">
-        <vue-tailwind-datepicker
-            v-model="modelValue" @change="$emit('updateModelValue', $event.target.value)" />
-    </template>
-    <template v-else-if="type === 'select' || type === 'multi_select'">
-        <select @change="$emit('updateModelValue', $event.target.value)" :multiple="type === 'multi_select'">
-            <option v-for="option in options" :value="option.id">{{ option.value }}</option>
-        </select>
-    </template>
+  </template>
+  <template v-else-if="type === 'date'">
+    <vue-tailwind-datepicker
+      v-model="modelValue"
+      @change="$emit('updateModelValue', $event.target.value)" />
+  </template>
+  <template v-else-if="type === 'select' || type === 'multi_select'">
+    <select
+      @change="$emit('updateModelValue', $event.target.value)"
+      :multiple="type === 'multi_select'">
+      <option v-for="option in options" :value="option.id">
+        {{ option.value }}
+      </option>
+    </select>
+  </template>
 </template>
 
 <script>
-
 import VueTailwindDatepicker from 'vue-tailwind-datepicker';
+import DataInputGroup from './DataInputGroup.vue';
 
 export default {
-    name: "CustomField",
-    components: {
-        VueTailwindDatepicker
+  name: 'CustomField',
+  components: {
+    VueTailwindDatepicker,
+    DataInputGroup,
+  },
+  props: {
+    name: {
+      type: String,
     },
-    props: {
-        name: {
-            type: String,
-        },
-        description: {
-            type: String
-        },
-        type: {
-            type: String,
-            default: 'text'
-        },
-        options: {
-            type: Object,
-            default: null
-        },
-        modelValue: {}
-    }
-}
+    description: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+    modelValue: {},
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
