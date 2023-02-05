7e0d69e573fa9ca17a01471b482704d8bb2592c8
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-02-05 13:00:25 -0800

    storybook

---
+++ resources/js/components/JovieSpinner.story.vue
@@ -0,0 +1,24 @@
+<template>
+  <JovieSpinner spinnerColor="orange" spinnerSize="md" />
+</template>
+<script>
+import JovieSpinner from './JovieSpinner.vue';
+
+export default {
+  story: {
+    category: 'Spinners',
+    name: 'JovieSpinner',
+  },
+  components: { JovieSpinner },
+  props: {
+    spinnerSize: {
+      type: String,
+      default: 'sm',
+    },
+    spinnerColor: {
+      type: String,
+      default: 'indigo',
+    },
+  },
+};
+</script>
