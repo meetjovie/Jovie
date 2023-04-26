35a77e006a3ca09073e8e8d55f2465b8dec05738
Aamish26622 <aamishirfan2662@gmail.com>
2023-04-26 14:19:05 +0500

    Code formated

--- app/Http/Controllers/TeamSettingController.php
+++ app/Http/Controllers/TeamSettingController.php
@@ -7,19 +7,21 @@ use Illuminate\Http\Request;
 
 class TeamSettingController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $settingKeys = TeamSetting::SETTING_KEYS;
+
         $teamSettings = TeamSetting::whereIn('key', $settingKeys)
             ->select(['key', 'value'])
             ->get()->toArray();
 
         $teamKeys = [];
-        foreach ($teamSettings as $teamSetting){
+        foreach ($teamSettings as $teamSetting) {
             $teamKeys[] = $teamSetting['key'];
         }
 
-        foreach ($settingKeys as $settingKey){
-            if(!in_array($settingKey, $teamKeys)){
+        foreach ($settingKeys as $settingKey) {
+            if (!in_array($settingKey, $teamKeys)) {
                 $teamSettings[] = [
                     'key' => $settingKey,
                     'value' => ''
@@ -33,11 +35,14 @@ class TeamSettingController extends Controller
         ]);
     }
 
-    public function update(Request $request){
+    public function update(Request $request)
+    {
         $updatedSettings = $request->teamSettings;
-        foreach ($updatedSettings as $updatedSetting){
+
+        foreach ($updatedSettings as $updatedSetting) {
             TeamSetting::setSetting($updatedSetting['key'], $updatedSetting['value']);
         }
+
         return response()->json([
             'status' => true,
             'data' => $updatedSettings,
--- resources/js/components/Account/AccountTeamSetting.vue
+++ resources/js/components/Account/AccountTeamSetting.vue
@@ -1,79 +1,59 @@
 <template>
-  <div>
-    <div v-if="onboarding">
-      <!-- Name -->
+  <div class="mx-auto max-w-7xl py-10 sm:px-6 lg:px-8">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="flex justify-between md:col-span-1">
+        <div class="px-4 sm:px-0">
+          <h3
+            class="text-lg font-medium text-slate-900 dark:text-jovieDark-100">
+            Team Settings
+          </h3>
 
-      <!-- Password -->
-      <div class="col-span-6 sm:col-span-4" v-if="!currentUser.password_set">
-        <InputGroup
-          @blur="setPassword()"
-          v-model="user.password"
-          :error="errors?.password?.[0]"
-          :disabled="updating"
-          passwordRevealToggle
-          name="set_password"
-          label="Set Password"
-          placeholder="Enter a Password" />
+          <p class="mt-1 text-sm text-slate-600 dark:text-jovieDark-300">
+            Update your team preferences
+          </p>
+        </div>
+
+        <div class="px-4 sm:px-0"></div>
+      </div>
+
+      <div class="mt-5 md:col-span-2 md:mt-0">
+        <div
+          x-data="{photoName: null, photoPreview: null}"
+          class="bg-white px-4 py-5 shadow dark:bg-jovieDark-800 sm:rounded-tl-md sm:rounded-tr-md sm:p-6">
+          <div class="grid grid-cols-6 gap-6">
+            <!-- Password -->
+            <div
+              v-for="(teamSetting, index) in teamSettings"
+              :key="index"
+              class="col-span-6 sm:col-span-4">
+              <InputGroup
+                v-model="teamSetting.value"
+                :disabled="updating"
+                :error="errors?.value?.[0]"
+                :name="teamSetting.key"
+                :label="teamSetting.key"
+                :placeholder="teamSetting.value" />
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="flex items-center justify-end bg-slate-50 px-4 py-3 text-right shadow dark:bg-jovieDark-900 dark:bg-jovieDark-900 sm:rounded-bl-md sm:rounded-br-md sm:px-6">
+          <ButtonGroup
+            type="submit"
+            design="primary"
+            text="Save"
+            @click="updateTeamSettings()"
+            :disabled="updating">
+          </ButtonGroup>
+        </div>
       </div>
     </div>
 
-    <div v-else class="mx-auto max-w-7xl py-10 sm:px-6 lg:px-8">
-      <div class="md:grid md:grid-cols-3 md:gap-6">
-        <div class="flex justify-between md:col-span-1">
-          <div class="px-4 sm:px-0">
-            <h3
-              class="text-lg font-medium text-slate-900 dark:text-jovieDark-100">
-              Team Settings
-            </h3>
-
-            <p class="mt-1 text-sm text-slate-600 dark:text-jovieDark-300">
-              Update your team preferences
-            </p>
-          </div>
-
-          <div class="px-4 sm:px-0"></div>
-        </div>
-
-        <div class="mt-5 md:col-span-2 md:mt-0">
-            <div
-              x-data="{photoName: null, photoPreview: null}"
-              class="bg-white px-4 py-5 shadow dark:bg-jovieDark-800 sm:rounded-tl-md sm:rounded-tr-md sm:p-6">
-              <div class="grid grid-cols-6 gap-6">
-                <!-- Password -->
-                <div
-                  v-for="(teamSetting, index) in teamSettings"
-                  :key="index"
-                  class="col-span-6 sm:col-span-4">
-                  <InputGroup
-                    v-model="teamSetting.value"
-                    :disabled="updating"
-                    :error="errors?.value?.[0]"
-                    :name="teamSetting.key"
-                    :label="teamSetting.key"
-                    :placeholder="teamSetting.value" />
-                </div>
-              </div>
-            </div>
-
-            <div
-              class="flex items-center justify-end bg-slate-50 px-4 py-3 text-right shadow dark:bg-jovieDark-900 dark:bg-jovieDark-900 sm:rounded-bl-md sm:rounded-br-md sm:px-6">
-              <ButtonGroup
-                type="submit"
-                design="primary"
-                text="Save"
-                @click="updateTeamSettings()"
-                :disabled="updating"
-                >
-              </ButtonGroup>
-            </div>
-        </div>
-      </div>
-
-      <div class="hidden sm:block">
-        <div class="py-8">
-          <div
-            class="border-t border-slate-200 dark:border-jovieDark-border"></div>
-        </div>
+    <div class="hidden sm:block">
+      <div class="py-8">
+        <div
+          class="border-t border-slate-200 dark:border-jovieDark-border"></div>
       </div>
     </div>
   </div>
@@ -85,18 +65,19 @@ import CardHeading from '../CardHeading.vue';
 import CardLayout from '../../components/CardLayout.vue';
 import ButtonGroup from '../../components/ButtonGroup.vue';
 import TeamService from '../../services/api/team.service';
+
 export default {
   name: 'AccountTeamSetting',
   components: { InputGroup, CardHeading, CardLayout, ButtonGroup },
   data() {
     return {
       errors: {},
-        updating: false,
-        setting: {
+      updating: false,
+      setting: {
         key: null,
         value: null,
       },
-        teamSettings: {},
+      teamSettings: {},
     };
   },
   props: {
@@ -132,7 +113,7 @@ export default {
     },
     updateTeamSettings() {
       this.updating = true;
-      let data = { teamSettings : this.teamSettings}
+      let data = { teamSettings: this.teamSettings };
       TeamService.updateTeamSettings(data)
         .then((response) => {
           response = response.data;
