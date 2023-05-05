b5d28d5cf9459ba23cbea86e819d802aa3f91227
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-05-05 11:49:02 -0700

    Improvement: Redesign new contact button

--- resources/js/views/Crm.vue
+++ resources/js/views/Crm.vue
@@ -4,24 +4,24 @@
       <div class="flex h-full w-full">
         <JovieSidebar @toggleShowSupportModal="toggleShowSupportModal()">
           <template #main>
-            <div class="">
+            <div class="mt-2">
               <div class="flex items-center text-xs">
                 <div class="mx-auto inline-flex">
                   <button
                     @click="openImportContactModal()"
                     type="button"
-                    class="rouned-md group relative mx-auto my-2 inline-flex cursor-pointer items-center justify-between rounded-l-md border bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-600 hover:bg-slate-200 dark:border-jovieDark-border dark:bg-jovieDark-border dark:text-jovieDark-300 hover:dark:bg-jovieDark-600">
+                    class="rouned-md group relative mx-auto inline-flex cursor-pointer items-center justify-between rounded-l-md border bg-slate-100 px-3 py-2 text-xs font-semibold text-slate-600 hover:bg-slate-200 dark:border-jovieDark-border dark:bg-jovieDark-border dark:text-jovieDark-300 hover:dark:bg-jovieDark-600">
                     <PlusIcon
-                      class="mr-1 h-5 w-5 items-center rounded-md p-1 text-xs text-purple-600 dark:text-purple-400"
+                      class="mr-1 h-4 w-4 items-center rounded-md text-xs text-purple-600 dark:text-purple-400"
                       aria-hidden="true" />
                     New Contact
                   </button>
                   <Menu>
                     <Float portal :offset="2" placement="bottom-start">
                       <MenuButton
-                        class="rouned-md group mx-auto my-2 flex cursor-pointer items-center justify-between rounded-r-md border bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-600 hover:bg-slate-200 dark:border-jovieDark-border dark:bg-jovieDark-border dark:text-jovieDark-300 hover:dark:bg-jovieDark-600">
+                        class="rouned-md group mx-auto flex cursor-pointer items-center justify-between rounded-r-md border bg-slate-100 px-2 py-2 text-xs font-semibold text-slate-600 hover:bg-slate-200 dark:border-jovieDark-border dark:bg-jovieDark-border dark:text-jovieDark-300 hover:dark:bg-jovieDark-600">
                         <ChevronDownIcon
-                          class="mr-1 h-5 w-5 items-center rounded-md p-1 text-xs text-purple-600 dark:text-purple-400"
+                          class="h-4 w-4 items-center rounded-md text-xs text-purple-600 dark:text-purple-400"
                           aria-hidden="true" />
                       </MenuButton>
                       <transition
