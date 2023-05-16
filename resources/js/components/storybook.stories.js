2bff717975c9df4d78d45885c5474b3963ae576e
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-05-15 21:02:13 -0700

    vite

--- resources/js/components/storybook.stories.js
+++ resources/js/components/storybook.stories.js
@@ -2,6 +2,15 @@
 import { buildStories } from '../../../.storybook/lib.js';
 
 const stories = require.context('./', true, /\.story\.vue$/);
+
 stories.keys().forEach((story) => {
   buildStories(stories(story).default);
 });
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Path/To/MyComponent',
+  component: MyComponent,
+};
