fda3ee5c3fae6d50fc098fcf440b89d1edcd908d
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-05-19 19:59:21 -0700

    Improvement: Various design improvements

--- resources/js/views/SettingsWorkspace.vue
+++ resources/js/views/SettingsWorkspace.vue
@@ -15,20 +15,20 @@
             <div
               v-for="item in stats"
               :key="item.name"
-              class="flex flex-col justify-between space-y-4 overflow-hidden rounded-lg border border-slate-300 px-4 py-5 dark:border-jovieDark-border sm:p-6">
+              class="flex flex-col space-y-4 overflow-hidden rounded-lg border border-slate-300 px-4 py-5 dark:border-jovieDark-border sm:p-6">
               <dt
                 class="truncate text-sm font-medium text-slate-900 dark:text-jovieDark-100">
                 {{ item.name }}
               </dt>
               <dd
-                class="mt-1 text-3xl font-semibold tracking-tight text-slate-900 dark:text-jovieDark-100">
+                class="mt-1 text-3xl font-semibold tracking-tight text-slate-900 dark:text-jovieDark-200">
                 {{ item.stat }} / {{ item.limit }}
               </dd>
               <ProgressBar
-                :size="sm"
+                size="xs"
                 :color="black"
                 :percentage="Math.round((item.stat / item.limit) * 100)" />
-              <span class="text-2xs text-slate-600">{{
+              <span class="text-2xs text-slate-600 dark:text-jovieDark-300">{{
                 item.description
               }}</span>
             </div>
