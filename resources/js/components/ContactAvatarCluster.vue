2ad91ae10bfbd81fbda36e8ee39a8acbc698f32b
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-05-20 17:08:45 -0700

    Fix: Fix various issues

--- resources/js/components/ContactAvatarCluster.vue
+++ resources/js/components/ContactAvatarCluster.vue
@@ -14,28 +14,11 @@ export default {
   components: {
     ContactAvatar,
   },
-  data() {
-    return {
-      contacts: [
-        {
-          name: 'Jane Cooper',
-          profile_pic_url:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        {
-          name: 'Kendall Jenner',
-          profile_pic_url:
-            'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-        },
-        {
-          name: 'Jessica Alba',
-          profile_pic_url:
-            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
-        },
-      ],
-    };
-  },
   props: {
+    contacts: {
+      type: Object,
+      required: true,
+    },
     editable: {
       type: Boolean,
       required: false,
