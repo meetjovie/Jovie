fb05cb2de3cc1c088a95686abc5add02627a1d42
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-05-20 22:05:43 -0700

    Improvemen: WIP Profile page

--- resources/js/views/SettingsProfile.vue
+++ resources/js/views/SettingsProfile.vue
@@ -1,19 +1,57 @@
 <template>
   <div>
     <SectionHeader header="Profile" subheader="Edit your user profile" />
-    <SectionWrapper header="Profile" subheader="Edit your profile">
-      <AccountProfile />
-    </SectionWrapper>
+    <AccountProfile />
+    <ActionPanel
+      v-if="!onboarding"
+      title="Delete Account"
+      buttonstyle="danger"
+      description="This is permanent and cannot be undone."
+      buttonText="Delete"
+      @action-click="toggleDeleteModal()" />
+    <ModalPopup
+      @primaryButtonClick="deleteAccount()"
+      :open="deleteModalOpen"
+      title="Deactivate account" />
   </div>
 </template>
 <script>
 import SectionHeader from './../components/SectionHeader.vue';
 import SectionWrapper from './../components/SectionWrapper.vue';
+import AccountProfile from './../components/Account/AccountProfile.vue';
+import ModalPopup from '../components/ModalPopup.vue';
+import ActionPanel from '../components/ActionPanel.vue';
 export default {
   name: 'SettingsProfile',
+  data() {
+    return {
+      deleteModalOpen: false,
+    };
+  },
+  methods: {
+    deleteAccount() {
+      console.log('deleteAccount');
+      this.$notify({
+        group: 'user',
+        type: 'success',
+        title: 'Deleting account',
+        text: 'This may take a few minutes.',
+      });
+      //add a function to cancel users current billing plan
+      //add a function to logout the user
+      //redirect user to the homepage
+      this.$router.push('/');
+    },
+    toggleDeleteModal() {
+      this.deleteModalOpen = !this.deleteModalOpen;
+    },
+  },
   components: {
     SectionHeader,
     SectionWrapper,
+    AccountProfile,
+    ModalPopup,
+    ActionPanel,
   },
 };
 </script>
