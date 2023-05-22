e31c6613a138af660f14098359a7119141004b41
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-05-21 23:34:37 -0700

    Design for calender settings

---
+++ resources/js/views/SettingsPreferences.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <SectionHeader header="Preferences" />
+    <SectionWrapper
+      header="Calender Events"
+      subheader="Set default settings for new calender events">
+      Default Calender Description Text go here
+    </SectionWrapper>
+  </div>
+</template>
+<script>
+import SectionHeader from '../components/SectionHeader.vue';
+import SectionWrapper from '../components/SectionWrapper.vue';
+export default {
+  components: {
+    SectionHeader,
+    SectionWrapper,
+  },
+};
+</script>
