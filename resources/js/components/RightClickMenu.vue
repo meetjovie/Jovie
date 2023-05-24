824faecb1605c52fa85b479076c82299ce889007
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-05-24 13:35:49 -0700

    rightmenuWIP

--- resources/js/components/RightClickMenu.vue
+++ resources/js/components/RightClickMenu.vue
@@ -5,20 +5,67 @@
     {{ show ? 'Hide' : 'Show' }} Menu
   </button>
   <teleport to="#crm">
-    <div
-      v-show="show"
-      :style="{ top: y + 'px', left: x + 'px' }"
-      style="z-index: 9999"
-      class="h-50 w-50 p-18 absolute rounded-full bg-blue-500">
-      {{ x }} x {{ y }}
-    </div>
+    <div>jooooooofd</div>
+    <Menu>
+      <TransitionRoot
+        :show="show"
+        enter-active-class="transition ease-out duration-100"
+        enter-from-class="transform opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-75"
+        leave-from-class="transform opacity-100 scale-100"
+        leave-to-class="transform opacity-0 scale-95">
+        <MenuItems>
+          <GlassmorphismContainer
+            :style="{ top: y + 'px', left: x + 'px' }"
+            style="z-index: 9999"
+            class="z-10 mt-2 w-48 origin-top-right px-1 py-1 ring-1 ring-black ring-opacity-5 focus-visible:outline-none">
+            <div class="py-1">
+              <ContactContextMenuItem
+                :contact="contact"
+                :contactMethods="[
+                  'email',
+                  'phone',
+                  'sms',
+                  'calendar',
+                  'instagram',
+
+                  'twitter',
+
+                  'whatsapp',
+                  'separator',
+                  'validate',
+                ]">
+              </ContactContextMenuItem>
+
+              <slot></slot>
+            </div>
+          </GlassmorphismContainer>
+        </MenuItems>
+      </TransitionRoot>
+    </Menu>
   </teleport>
 </template>
 <script>
 import ContactContextMenu from './ContactContextMenu.vue';
+import ContactContextMenuItem from './ContactContextMenuItem.vue';
 import DropdownMenuItem from './DropdownMenuItem.vue';
+import { Menu, MenuButton, MenuItems, TransitionRoot } from '@headlessui/vue';
+import { CloudArrowDownIcon } from '@heroicons/vue/24/solid';
+import GlassmorphismContainer from './GlassmorphismContainer.vue';
+
 export default {
-  components: { ContactContextMenu, DropdownMenuItem },
+  components: {
+    ContactContextMenu,
+    ContactContextMenuItem,
+    DropdownMenuItem,
+    Menu,
+    MenuButton,
+    MenuItems,
+    TransitionRoot,
+    CloudArrowDownIcon,
+    GlassmorphismContainer,
+  },
   methods: {
     hideMenu() {
       console.log('hide menu');
@@ -35,11 +82,11 @@ export default {
   props: {
     contact: {
       type: Object,
-      required: true,
+      required: false,
     },
     show: {
       type: Boolean,
-      default: true,
+      default: false,
     },
     x: {
       type: Number,
