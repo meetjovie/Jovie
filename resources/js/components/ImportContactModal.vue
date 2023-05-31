90ab6fc619e3eef1f3aa24e4be8bfbf117043d69
Tim White <35063371+itstimwhite@users.noreply.github.com>
2023-05-30 17:43:55 -0700

    design

--- resources/js/components/ImportContactModal.vue
+++ resources/js/components/ImportContactModal.vue
@@ -68,24 +68,29 @@
                               <XMarkIcon class="h-6 w-6" aria-hidden="true" />
                             </button>
                           </div>
-                          <div class="mt-4 flex items-center px-4">
-                            <ContactAvatar
-                              @updateAvatar="contact.profile_pic = $event"
-                              :contact="contact"
-                              :editable="true"
-                              class="h-14 w-14 flex-shrink-0 text-gray-300 sm:-ml-1" />
-                            <label for="first_name" class="sr-only"
-                              >First Name</label
-                            >
-                            <input
-                              type="text"
-                              :disabled="importing"
-                              name="first_name"
-                              tabindex="-1"
-                              id="first_name"
-                              class="block w-28 border-0 bg-transparent pt-2.5 text-lg font-bold tracking-tight placeholder-shown:w-28 placeholder-shown:text-slate-400 focus:ring-0 focus:placeholder:text-slate-300"
-                              placeholder="First Name"
-                              v-model="contact.first_name" />
+                          <div class="mt-4 flex w-full items-center px-4">
+                            <div class="px-2">
+                              <ContactAvatar
+                                @updateAvatar="contact.profile_pic = $event"
+                                :contact="contact"
+                                :editable="true"
+                                class="h-14 w-14 flex-shrink-0 text-gray-300" />
+                            </div>
+
+                            <div class="">
+                              <label for="first_name" class="sr-only"
+                                >First Name</label
+                              >
+                              <input
+                                type="text"
+                                :disabled="importing"
+                                name="first_name"
+                                tabindex="-1"
+                                id="first_name"
+                                class="z-20 block w-32 border-0 bg-transparent px-4 pt-2.5 text-lg font-bold tracking-tight placeholder-shown:w-32 placeholder-shown:text-slate-400 focus:ring-0 focus:placeholder:text-slate-300"
+                                placeholder="First Name"
+                                v-model="contact.first_name" />
+                            </div>
 
                             <label for="first_name" class="sr-only"
                               >Last Name</label
@@ -101,7 +106,7 @@
                               v-model="contact.last_name" />
                           </div>
                           <div
-                            class="h-6 py-1 text-xs text-red-600 dark:text-red-400">
+                            class="h-6 w-full justify-start py-1 text-xs text-red-600 dark:text-red-400">
                             <span v-if="errors.first_name">
                               {{ errors.first_name[0] }}</span
                             >
@@ -154,7 +159,7 @@
                           <div
                             class="flex items-center justify-between space-x-4 border-t border-gray-200 px-2 py-2">
                             <div
-                              v-if="enrichable"
+                              v-if="!enrichable"
                               class="flex w-full justify-end space-x-2">
                               <ButtonGroup
                                 :loader="importing"
@@ -372,6 +377,10 @@ export default {
     },
   },
   computed: {
+    firstNameWidth() {
+      // Calculate the width based on the length of this.contact.first_name
+      return Math.max(this.contact.first_name.length * 8, 14);
+    },
     contactFields() {
       return this.fields.filter(
         (field) =>
